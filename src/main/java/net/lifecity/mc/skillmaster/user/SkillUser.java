75ecfbe329249f86ba98398b15dfb27ccd633818
Koryu25 <komu.ryu@icloud.com>
2022-07-20 21:46:03 +0900

    SkillUser.reinforceを追加

--- src/main/java/net/lifecity/mc/skillmaster/user/SkillUser.java
+++ src/main/java/net/lifecity/mc/skillmaster/user/SkillUser.java
@@ -25,9 +25,14 @@ public class SkillUser {
 
     /**
      * 武器を構えます
+     * ケース１:お互いが構えた状態で接する
+     * ケース２:どちらかが構えた時、すでに接している状態
+     * 攻撃失敗 and 防御成功
+     * ケース３:Aが構えているが、Bが構えていない
+     * A攻撃成功 and B防御失敗
      */
     public void reinforce() {
-        if (!canBeReinforced) { //武器を構えられるか
+        if (!canBeReinforced) { //クールタイム確認
             playSound(Sound.ENTITY_PLAYER_ATTACK_NODAMAGE);
             if (reinforced)
                 sendMessage("すでに武器を構えている");
@@ -35,33 +40,36 @@ public class SkillUser {
                 sendMessage("まだ武器を構えられない");
         }
 
-        reinforced = true; //武器を構える
-        canBeReinforced = false; //一定時間武器を構えられなくなる
-        //スキルポイント消費
+        new Reinforce(3, 5); //処理開始
+    }
 
-        sendMessage("武器を構えた"); //ログ
+    private class Reinforce extends BukkitRunnable {
 
-        attack();
+        private int hold;
+        private int next;
 
-        new BukkitRunnable() { //時間経過で構えが崩れる
-            @Override
-            public void run() {
-                //メイン処理
-                reinforced = false;
+        private int tick = 0;
 
-                //演出
-                playSound(Sound.ENTITY_PLAYER_ATTACK_STRONG);
-                sendMessage("構えが崩れた");
-            }
-        }.runTaskTimer(SkillMaster.instance, 0, 3);
+        public Reinforce(int hold, int next) {
+            this.hold = hold;
+            this.next = next;
 
+            reinforced = true; //武器を構える
+            canBeReinforced = false; //一定時間武器を構えられなくなる
 
-        new BukkitRunnable() { //一定時間後、再び構えられるようになる
-            @Override
-            public void run() {
-                canBeReinforced = true;
-            }
-        }.runTaskTimer(SkillMaster.instance, 0, 5);
+            sendMessage("武器を構えた");
+
+            runTaskTimer(SkillMaster.instance, 0, next);
+        }
+
+        @Override
+        public void run() {
+            //タイマー:近くに敵がいたら
+            // 敵が武器を構えていたら防御
+            // 構えていなかったら待機
+            //タイマー切れ:近くに敵がいたら攻撃
+            tick--;
+        }
     }
 
     /**
@@ -71,6 +79,17 @@ public class SkillUser {
      * todo 複数の標的に攻撃
      */
     public void attack() {
+        List<Entity> targets = player.getNearbyEntities(1.5, 1.5, 1.5);
+    }
+
+    /**
+     * このプレイヤーの位置から一番近いentityを取得します
+     * @return このプレイヤーの位置から一番近いentity
+     */
+    private Entity getNearestEntityByLocation(double radius) {
+        List<Entity> entities = player.getNearbyEntities(radius, radius, radius);
+        if (entities.size() == 0) return null;
+        return entities.get(0);
     }
 
     /**
