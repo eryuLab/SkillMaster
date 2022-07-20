3765c8e16e3121181353ac729d7474abb48adc3a
Koryu25 <komu.ryu@icloud.com>
2022-07-20 21:44:45 +0900

    メインクラスのインスタンスに参照できるように変更

--- src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
+++ src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
@@ -1,13 +1,15 @@
 package net.lifecity.mc.skillmaster;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkillMaster extends JavaPlugin {
 
+    public static SkillMaster instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
     }
 
     @Override
