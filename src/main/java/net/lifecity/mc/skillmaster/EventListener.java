997de97b6ea7d4de85dbb712e015114a45edb1f7
Koryu25 <komu.ryu@icloud.com>
2022-07-21 16:05:10 +0900

    素早く移動する動作を追加

--- src/main/java/net/lifecity/mc/skillmaster/EventListener.java
+++ src/main/java/net/lifecity/mc/skillmaster/EventListener.java
@@ -1,10 +1,12 @@
 package net.lifecity.mc.skillmaster;
 
 import net.lifecity.mc.skillmaster.user.SkillUser;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -25,13 +27,25 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         SkillUser user = SkillMaster.instance.getUserList().get(event.getPlayer());
-        if (event.getAction().isLeftClick())
-            user.reinforce();
+
+        if (user.getHandMaterial() == Material.WOODEN_SWORD) { //木の剣を持っているときだけ
+
+            if (event.getAction().isLeftClick()) //武器を構える
+                user.reinforce();
+
+            else if (event.getAction().isRightClick()) //素早く移動する
+                user.moveQuickly();
+        }
     }
 
     @EventHandler
-    public void onEntityDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player)
-            event.setCancelled(true);
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        // プレイヤーが木の剣で攻撃したらイベントキャンセル
+        if (event.getDamager() instanceof Player player) {
+            SkillUser user = SkillMaster.instance.getUserList().get(player);
+            if (user.getHandMaterial() == Material.WOODEN_SWORD) {
+                event.setCancelled(true);
+            }
+        }
     }
 }
