9ca45dde7e46279364775919fe6ef720d0f02ba3
Koryu25 <komu.ryu@icloud.com>
2022-07-22 02:43:02 +0900

    ActionableSkillの追加

--- src/main/java/net/lifecity/mc/skillmaster/skill/Skill.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/Skill.java
@@ -2,7 +2,6 @@ package net.lifecity.mc.skillmaster.skill;
 
 import lombok.Getter;
 import net.lifecity.mc.skillmaster.user.SkillUser;
-import org.bukkit.event.block.Action;
 
 public abstract class Skill {
 
@@ -12,12 +11,10 @@ public abstract class Skill {
     @Getter
     private final int point;
 
-    public Skill(String name, int point) {
+    protected Skill(String name, int point) {
         this.name = name;
         this.point = point;
     }
 
     public abstract void activate(SkillUser user);
-
-    public abstract void action(SkillUser user);
 }
