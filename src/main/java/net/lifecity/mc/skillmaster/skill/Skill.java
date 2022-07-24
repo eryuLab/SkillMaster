36a83f2bee3c692860635565a0c1b106b836232b
Koryu25 <komu.ryu@icloud.com>
2022-07-25 00:36:28 +0900

    [update]: SkillとActionableSkillとUserSkillの統合

--- src/main/java/net/lifecity/mc/skillmaster/skill/Skill.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/Skill.java
@@ -1,24 +1,60 @@
 package net.lifecity.mc.skillmaster.skill;
 
 import lombok.Getter;
+import net.lifecity.mc.skillmaster.SkillMaster;
 import net.lifecity.mc.skillmaster.user.SkillUser;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public abstract class Skill {
 
     @Getter
-    private final String name;
+    protected final String name;
 
     @Getter
-    private final int point;
+    protected final int point;
 
     @Getter
-    private final int interval;
+    protected final int interval;
 
-    protected Skill(String name, int point, int interval) {
+    protected final SkillUser user;
+
+    protected boolean activating = false;
+
+    protected Skill(String name, int point, int interval, SkillUser user) {
         this.name = name;
         this.point = point;
         this.interval = interval;
+        this.user = user;
     }
 
-    public abstract void activate(SkillUser user);
+    /**
+     * スキルを発動します
+     */
+    public void activate() {
+        // インターバル中か確認
+        if (activating)
+            return;
+
+        // インターバル開始
+        activating = true;
+
+        // 終了処理
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                deactivate();
+            }
+        }.runTaskLater(SkillMaster.instance, interval);
+    }
+
+    /**
+     * スキルを終了します
+     */
+    public void deactivate() {
+        if (!activating)
+            return;
+
+        activating = false;
+    }
+
 }
