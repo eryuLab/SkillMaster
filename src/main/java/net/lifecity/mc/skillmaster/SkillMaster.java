bd99a6605840f8760fca8f8d7fc1465f7d5d19d7
Koryu25 <komu.ryu@icloud.com>
2022-07-29 15:16:20 +0900

    [update]: メインクラスでWeaponListをインスタンス生成

--- src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
+++ src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
@@ -4,6 +4,7 @@ import dev.jorel.commandapi.CommandAPI;
 import lombok.Getter;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.user.SkillUserList;
+import net.lifecity.mc.skillmaster.weapon.WeaponList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -16,6 +17,9 @@ public final class SkillMaster extends JavaPlugin {
     @Getter
     private SkillUserList userList;
 
+    @Getter
+    private WeaponList weaponList;
+
     @Override
     public void onEnable() {
         instance = this;
@@ -25,6 +29,8 @@ public final class SkillMaster extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
 
         userList = new SkillUserList();
+
+        weaponList = new WeaponList();
     }
 
     @Override
