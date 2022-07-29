bef8d948705166d5c91884affd56749e7272c2c8
Koryu25 <komu.ryu@icloud.com>
2022-07-29 15:31:33 +0900

    [update]: メインクラスからWeaponListの記述を削除

--- src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
+++ src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
@@ -2,13 +2,8 @@ package net.lifecity.mc.skillmaster;
 
 import dev.jorel.commandapi.CommandAPI;
 import lombok.Getter;
-import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.user.SkillUserList;
-import net.lifecity.mc.skillmaster.weapon.WeaponList;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class SkillMaster extends JavaPlugin {
 
@@ -16,9 +11,6 @@ public final class SkillMaster extends JavaPlugin {
 
     @Getter
     private SkillUserList userList;
-
-    @Getter
-    private WeaponList weaponList;
 
     @Override
     public void onEnable() {
@@ -29,8 +21,6 @@ public final class SkillMaster extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
 
         userList = new SkillUserList();
-
-        weaponList = new WeaponList();
     }
 
     @Override
