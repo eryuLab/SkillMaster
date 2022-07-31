f3a4cbd0ac8fdd52b4918e112c99ec4bec18ff29
Koryu25 <komu.ryu@icloud.com>
2022-07-31 15:51:44 +0900

    [add]: スキルクラスMoveFastの追加

---
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/MoveFast.java
@@ -0,0 +1,26 @@
+package net.lifecity.mc.skillmaster.skill.skills;
+
+import net.lifecity.mc.skillmaster.skill.Skill;
+import net.lifecity.mc.skillmaster.user.SkillUser;
+import net.lifecity.mc.skillmaster.weapon.Weapon;
+import org.bukkit.util.Vector;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MoveFast extends Skill {
+
+
+    protected MoveFast(SkillUser user) {
+        super("高速移動", Arrays.asList(Weapon.STRAIGHT_SWORD, Weapon.DAGGER, Weapon.LONG_SWORD, Weapon.RAPIER), 0, 25, user);
+    }
+
+    @Override
+    public void activate() {
+        super.activate();
+
+        Vector vector = user.getPlayer().getEyeLocation().getDirection();
+
+        user.getPlayer().setVelocity(vector);
+    }
+}
