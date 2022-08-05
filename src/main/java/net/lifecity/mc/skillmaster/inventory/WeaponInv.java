242d7703bcaacc4bf609b0ed2ee8ba07bcda0175
Koryu25 <komu.ryu@icloud.com>
2022-08-05 20:14:22 +0900

    [update]: WeaponInvにEdgeを追加

--- src/main/java/net/lifecity/mc/skillmaster/inventory/WeaponInv.java
+++ src/main/java/net/lifecity/mc/skillmaster/inventory/WeaponInv.java
@@ -1,13 +1,22 @@
 package net.lifecity.mc.skillmaster.inventory;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.pane.Pane;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.weapon.Weapon;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class WeaponInv {
 
     public WeaponInv(SkillUser user) {
         ChestGui gui = new ChestGui(3, "武器メニュー");
 
+    }
+
+    private static StaticPane Edge() {
+        StaticPane pane = new StaticPane(0, 0, 1, 3);
     }
 }
