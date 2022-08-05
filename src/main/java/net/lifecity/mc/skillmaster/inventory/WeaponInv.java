7eda9a599f2e21c9ae7090160db807a8eb2ba8e0
Koryu25 <komu.ryu@icloud.com>
2022-08-05 20:01:43 +0900

    [update]: WeaponInvにコンストラクタを追加

--- src/main/java/net/lifecity/mc/skillmaster/inventory/WeaponInv.java
+++ src/main/java/net/lifecity/mc/skillmaster/inventory/WeaponInv.java
@@ -1,4 +1,13 @@
 package net.lifecity.mc.skillmaster.inventory;
 
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import net.lifecity.mc.skillmaster.user.SkillUser;
+import net.lifecity.mc.skillmaster.weapon.Weapon;
+
 public class WeaponInv {
+
+    public WeaponInv(SkillUser user) {
+        ChestGui gui = new ChestGui(3, "武器メニュー");
+
+    }
 }
