27cbc8bd931d49367af26c9a4c6ba80e528c176b
Koryu25 <komu.ryu@icloud.com>
2022-08-13 13:51:53 +0900

    [update]: 大ジャンプをスキルセットに追加

--- src/main/java/net/lifecity/mc/skillmaster/user/SkillUser.java
+++ src/main/java/net/lifecity/mc/skillmaster/user/SkillUser.java
@@ -7,6 +7,7 @@ import net.lifecity.mc.skillmaster.skill.DefenseSkill;
 import net.lifecity.mc.skillmaster.skill.SeparatedSkill;
 import net.lifecity.mc.skillmaster.skill.Skill;
 import net.lifecity.mc.skillmaster.skill.separatedskills.straightsword.SSDefense;
+import net.lifecity.mc.skillmaster.skill.skills.straightsword.SSHighJump;
 import net.lifecity.mc.skillmaster.skill.skills.straightsword.SSVectorAttack;
 import net.lifecity.mc.skillmaster.skill.skills.straightsword.SSMoveFast;
 import net.lifecity.mc.skillmaster.utils.EntitySort;
@@ -67,7 +68,7 @@ public class SkillUser {
         };
         this.dropSkillSet = new Skill[] {
                 new SSDefense(this),
-                null,
+                new SSHighJump(this),
                 null
         };
     }
