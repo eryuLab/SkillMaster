1cd1c83f5d020ed38387fa6f0c0f59fcef648b3f
Koryu25 <komu.ryu@icloud.com>
2022-08-15 12:39:41 +0900

    [add]: SkillKeyの追加

--- src/main/java/net/lifecity/mc/skillmaster/user/SkillKey.java
+++ src/main/java/net/lifecity/mc/skillmaster/user/SkillKey.java
@@ -1,7 +1,38 @@
 package net.lifecity.mc.skillmaster.user;
 
-public enum SkillKey {
-    RIGHT,
-    SWAP,
-    DROP
+import lombok.Getter;
+
+public class SkillKey {
+
+    // 右クリックの入力
+    public static final SkillKey RIGHT_ONE = new SkillKey(SkillButton.RIGHT, 0, 10);
+    public static final SkillKey RIGHT_TWO = new SkillKey(SkillButton.RIGHT, 1, 19);
+    public static final SkillKey RIGHT_THREE = new SkillKey(SkillButton.RIGHT, 2, 28);
+
+    // スワップキーの入力
+    public static final SkillKey SWAP_ONE = new SkillKey(SkillButton.SWAP, 0, 11);
+    public static final SkillKey SWAP_TWO = new SkillKey(SkillButton.SWAP, 1, 20);
+    public static final SkillKey SWAP_THREE = new SkillKey(SkillButton.SWAP, 2, 29);
+
+    // ドロップキーの入力
+    public static final SkillKey DROP_ONE = new SkillKey(SkillButton.DROP, 0, 12);
+    public static final SkillKey DROP_TWO = new SkillKey(SkillButton.DROP, 1, 21);
+    public static final SkillKey DROP_THREE = new SkillKey(SkillButton.DROP, 2, 30);
+
+    private final SkillButton button;
+    private final int num;
+    @Getter
+    private final int invSlot;
+
+    private SkillKey(SkillButton button, int num, int invSlot) {
+        this.button = button;
+        this.num = num;
+        this.invSlot = invSlot;
+    }
+
+    private enum SkillButton {
+        RIGHT,
+        SWAP,
+        DROP
+    }
 }
