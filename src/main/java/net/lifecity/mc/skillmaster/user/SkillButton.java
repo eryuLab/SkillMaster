d404fa3774d657e728564522a7faf5657d21f82e
Koryu25 <komu.ryu@icloud.com>
2022-08-16 14:07:59 +0900

    [add]: SkillButtonの追加

---
+++ src/main/java/net/lifecity/mc/skillmaster/user/SkillButton.java
@@ -0,0 +1,19 @@
+package net.lifecity.mc.skillmaster.user;
+
+import lombok.Getter;
+
+/**
+ * スキルに使うボタンの列挙
+ */
+public enum SkillButton {
+    RIGHT("右クリック"),
+    SWAP("スワップ"),
+    DROP("ドロップ");
+
+    @Getter
+    private String jp;
+
+    SkillButton(String jp) {
+        this.jp = jp;
+    }
+}
