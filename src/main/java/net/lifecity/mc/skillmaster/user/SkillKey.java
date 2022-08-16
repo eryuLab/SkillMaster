641e76a36590c7c5cebd788d6cad14b17c5892bf
Koryu25 <komu.ryu@icloud.com>
2022-08-16 14:23:29 +0900

    [add]: SkillKeyの追加

--- src/main/java/net/lifecity/mc/skillmaster/user/SkillKey.java
+++ src/main/java/net/lifecity/mc/skillmaster/user/SkillKey.java
@@ -1,58 +1,26 @@
 package net.lifecity.mc.skillmaster.user;
 
 import lombok.Getter;
-import org.bukkit.Material;
+import lombok.Setter;
+import net.lifecity.mc.skillmaster.skill.Skill;
 
 /**
- * スキルを入力するためのキーについて機能をまとめたクラス
+ * 入力に使うスキルキーのクラス
  */
 public class SkillKey {
-
-    // ドロップキーの入力
-    public static final SkillKey DROP_ONE = new SkillKey(SkillButton.DROP, 0, 10);
-    public static final SkillKey DROP_TWO = new SkillKey(SkillButton.DROP, 1, 19);
-    public static final SkillKey DROP_THREE = new SkillKey(SkillButton.DROP, 2, 28);
-
-    // スワップキーの入力
-    public static final SkillKey SWAP_ONE = new SkillKey(SkillButton.SWAP, 0, 12);
-    public static final SkillKey SWAP_TWO = new SkillKey(SkillButton.SWAP, 1, 21);
-    public static final SkillKey SWAP_THREE = new SkillKey(SkillButton.SWAP, 2, 30);
-
-    // 右クリックの入力
-    public static final SkillKey RIGHT_ONE = new SkillKey(SkillButton.RIGHT, 0, 14);
-    public static final SkillKey RIGHT_TWO = new SkillKey(SkillButton.RIGHT, 1, 23);
-    public static final SkillKey RIGHT_THREE = new SkillKey(SkillButton.RIGHT, 2, 32);
 
     @Getter
     private final SkillButton button;
     @Getter
     private final int num;
-    @Getter
-    private final int invSlot;
 
-    private SkillKey(SkillButton button, int num, int invSlot) {
+    @Getter
+    @Setter
+    private Skill skill;
+
+    private SkillKey(SkillButton button, int num, Skill skill) {
         this.button = button;
         this.num = num;
-        this.invSlot = invSlot;
-    }
-
-    /**
-     * スキル入力キーの列挙
-     */
-    public enum SkillButton {
-        RIGHT(Material.YELLOW_STAINED_GLASS_PANE, "右クリック"),
-        SWAP(Material.LIGHT_BLUE_STAINED_GLASS_PANE, "スワップ"),
-        DROP(Material.ORANGE_STAINED_GLASS_PANE, "ドロップ");
-
-        @Getter
-        private Material material;
-
-        @Getter
-        private String jp;
-
-        SkillButton(Material material, String jp) {
-            this.material = material;
-            this.jp = jp;
-        }
+        this.skill = skill;
     }
 }
