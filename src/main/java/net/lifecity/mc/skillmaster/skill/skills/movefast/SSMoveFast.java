9eae162685302ed8150226e5eed067effae928de
Koryu25 <komu.ryu@icloud.com>
2022-08-25 15:17:22 +0900

    [update]: SSMoveFastにLEを追加

--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/movefast/SSMoveFast.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/movefast/SSMoveFast.java
@@ -1,9 +1,13 @@
 package net.lifecity.mc.skillmaster.skill.skills.movefast;
 
+import net.lifecity.mc.skillmaster.SkillMaster;
 import net.lifecity.mc.skillmaster.skill.Skill;
 import net.lifecity.mc.skillmaster.skill.SkillType;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.weapon.Weapon;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.Arrays;
@@ -28,5 +32,24 @@ public class SSMoveFast extends Skill {
             vector.setY(0.15);
 
         user.getPlayer().setVelocity(vector);
+
+        // 軌道
+        new BukkitRunnable() {
+            int count = 0;
+            @Override
+            public void run() {
+                if (count >= 7)
+                    cancel();
+                if (user.getPlayer().getVelocity().length() < 0.3)
+                    cancel();
+                if (count % 2 == 1)
+                particle(Particle.FALLING_DUST, user.getPlayer().getLocation(), Material.OXIDIZED_COPPER.createBlockData());
+                if (count %  2 == 0)
+                    particle(Particle.FALLING_DUST, user.getPlayer().getLocation(), Material.IRON_BLOCK.createBlockData());
+                if (count % 3 == 0)
+                    particle(Particle.FALLING_DUST, user.getPlayer().getLocation(), Material.ICE.createBlockData());
+                count++;
+            }
+        }.runTaskTimer(SkillMaster.instance, 0, 1);
     }
 }
