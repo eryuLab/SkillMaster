2f81244c5e5d828d5d0d899eaf8f6cce8e1ad332
Koryu25 <komu.ryu@icloud.com>
2022-08-25 14:44:34 +0900

    [update]: BlockDataを必要とするparticle()を追加

--- src/main/java/net/lifecity/mc/skillmaster/skill/Skill.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/Skill.java
@@ -8,6 +8,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -87,6 +88,16 @@ public class Skill {
      */
     protected void particle(Particle particle, Location location) {
         user.getPlayer().getWorld().spawnParticle(particle, location, 1);
+    }
+
+    /**
+     * パーティクルを表示します
+     * @param particle 表示するパーティクル
+     * @param location 表示する位置
+     * @param blockData パーティクルのデータ
+     */
+    protected void particle(Particle particle, Location location, BlockData blockData) {
+        user.getPlayer().getWorld().spawnParticle(particle, location, 1, blockData);
     }
 
     /**
