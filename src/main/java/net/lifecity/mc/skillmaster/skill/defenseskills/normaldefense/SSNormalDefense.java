0552af7231354cc98e8ae500e94b9c6da460bea7
Koryu25 <komu.ryu@icloud.com>
2022-08-27 15:42:47 +0900

    [update]: SSNormalDefenseへNormalDefenseを適応

--- src/main/java/net/lifecity/mc/skillmaster/skill/defenseskills/normaldefense/SSNormalDefense.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/defenseskills/normaldefense/SSNormalDefense.java
@@ -1,65 +1,14 @@
 package net.lifecity.mc.skillmaster.skill.defenseskills.normaldefense;
 
-import net.lifecity.mc.skillmaster.SkillMaster;
-import net.lifecity.mc.skillmaster.skill.DefenseSkill;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.weapon.Weapon;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
-
-import java.util.Arrays;
-import java.util.Random;
 
 /**
  * 0.5秒間だけ敵からの攻撃を完全に防御するスキル
  */
-public class SSNormalDefense extends DefenseSkill {
+public class SSNormalDefense extends NormalDefense {
 
     public SSNormalDefense(SkillUser user) {
-        super("通常防御", Weapon.STRAIGHT_SWORD, Arrays.asList("基本的な防御姿勢になります。", "発動してから一瞬だけすべての攻撃を防ぐことができます。"), 5, 0, 10, 35, user);
-    }
-
-    @Override
-    public void defense(double damage, Vector vector) {
-        user.playSound(Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
-
-        // ガードエフェクト
-        for (int i = 0; i < 2; i++) {
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    for (int j = 0; j < 4; j++) {
-                        // 座標を生成
-                        double max = 1.5;
-                        Random random = new Random();
-
-                        double x = max * random.nextDouble();
-                        if (random.nextBoolean())
-                            x *= -1;
-
-                        double y = max * random.nextDouble();
-                        if (random.nextBoolean())
-                            y *= -1;
-
-                        double z = max * random.nextDouble();
-                        if (random.nextBoolean())
-                            z *= -1;
-
-                        Location loc = user.getPlayer().getEyeLocation().add(x, y, z);
-
-                        particle(Particle.FALLING_DUST, loc, Material.ICE.createBlockData());
-
-                        if (j % 2 == 0)
-                            particle(Particle.ELECTRIC_SPARK, loc);
-                        if (j % 3 == 0)
-                            particle(Particle.LAVA, loc);
-                    }
-                }
-            }.runTaskLater(SkillMaster.instance, i * 4);
-        }
+        super(Weapon.STRAIGHT_SWORD, 5, 0, 10, 35, user);
     }
 }
