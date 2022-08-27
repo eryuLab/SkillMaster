f43413771abae98c22889fd8f715e1178e6fc009
Koryu25 <komu.ryu@icloud.com>
2022-08-27 18:41:03 +0900

    [update]: VectorAttackに対応

--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/vectorattack/SSVectorAttack.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/vectorattack/SSVectorAttack.java
@@ -17,62 +17,21 @@ import java.util.Arrays;
 /**
  * 自分のベクトルの強さを攻撃力に変換して攻撃するスキル
  */
-public class SSVectorAttack extends Skill {
+public class SSVectorAttack extends VectorAttack {
 
-    public SSVectorAttack(SkillUser user) {
-        super("ベクトルアタック", Weapon.STRAIGHT_SWORD, SkillType.ATTACK, Arrays.asList("ユーザーが持つベクトルを力に変換して攻撃します。"), 3, 0, 40, user);
-    }
-
-    @Override
-    public void activate() {
-        super.activate();
-
-        Vector vector = user.getPlayer().getEyeLocation().getDirection().multiply(1.2);
-
-        user.getPlayer().setVelocity(user.getPlayer().getVelocity().add(vector));
-
-        double damage = user.getPlayer().getVelocity().length();
-        damage *= 2;
-
-        boolean b = user.attackNearest(
+    public SSVectorAttack(SkillUser user, int num) {
+        super(
+                Weapon.STRAIGHT_SWORD,
+                num,
+                0,
+                40,
+                user,
+                1.2,
+                2,
                 1.8,
-                damage,
-                user.getPlayer().getVelocity().multiply(0.25).setY(0.15),
-                Sound.ENTITY_PLAYER_ATTACK_CRIT
+                0.25,
+                0.15,
+                5
         );
-
-        if (b)
-            user.sendMessage("damage: " + damage);
-
-        // 軌道
-        double finalDamage = damage;
-        BlockData data;
-        if (finalDamage > 5)
-            data = Material.RED_CONCRETE_POWDER.createBlockData();
-        else if (finalDamage > 4)
-            data = Material.PURPLE_CONCRETE_POWDER.createBlockData();
-        else if (finalDamage > 3)
-            data = Material.MAGENTA_CONCRETE_POWDER.createBlockData();
-        else if (finalDamage > 2)
-            data = Material.PINK_CONCRETE_POWDER.createBlockData();
-        else
-            data = Material.WHITE_CONCRETE_POWDER.createBlockData();
-
-        new BukkitRunnable() {
-            int count = 0;
-            @Override
-            public void run() {
-                if (count >= 8)
-                    cancel();
-                if (user.getPlayer().getVelocity().length() < 0.47)
-                    cancel();
-                particle(Particle.FALLING_DUST, user.getPlayer().getLocation(), data);
-                if (finalDamage > 5)
-                    particle(Particle.LAVA, user.getPlayer().getLocation());
-                else if (finalDamage > 4 && count % 2 == 1)
-                    particle(Particle.LAVA, user.getPlayer().getLocation());
-                count++;
-            }
-        }.runTaskTimer(SkillMaster.instance, 0, 1);
     }
 }
