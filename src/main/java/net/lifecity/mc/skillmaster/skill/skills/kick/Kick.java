bd8c6c681f134e8d4adb8268856ace2f0d335ba5
Koryu25 <komu.ryu@icloud.com>
2022-08-27 17:13:19 +0900

    [add]: Kickの追加

---
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/kick/Kick.java
@@ -0,0 +1,75 @@
+package net.lifecity.mc.skillmaster.skill.skills.kick;
+
+import net.lifecity.mc.skillmaster.SkillMaster;
+import net.lifecity.mc.skillmaster.skill.Skill;
+import net.lifecity.mc.skillmaster.skill.SkillType;
+import net.lifecity.mc.skillmaster.user.SkillUser;
+import net.lifecity.mc.skillmaster.weapon.Weapon;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Arrays;
+
+/**
+ * 敵を蹴り飛ばすスキル
+ */
+public class Kick extends Skill {
+
+    private final double attackRadius;
+    private final double impact;
+
+    public Kick(
+            Weapon weapon,
+            int num,
+            int point,
+            int interval,
+            SkillUser user,
+            double attackRadius,
+            double impact
+    ) {
+        super(
+                "蹴り",
+                weapon,
+                SkillType.ATTACK,
+                Arrays.asList("相手を蹴り飛ばして、相手と距離を作ります。", "一番近い敵を攻撃します。"),
+                num,
+                point,
+                interval,
+                user
+        );
+        this.attackRadius = attackRadius;
+        this.impact = impact;
+    }
+
+    @Override
+    public void activate() {
+
+        boolean b = user.attackNearest(
+                attackRadius,
+                1,
+                user.getPlayer().getEyeLocation().getDirection().multiply(impact),
+                Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK
+        );
+
+        if (b) {
+            Entity entity = user.getNearEntities(attackRadius).get(0);
+            new BukkitRunnable() {
+                int count = 0;
+                @Override
+                public void run() {
+                    if (count >= 12)
+                        cancel();
+                    if (entity.getVelocity().length() <= 0.4)
+                        cancel();
+
+                    particle(Particle.FALLING_DUST, entity.getLocation().add(0, 1, 0), Material.ICE.createBlockData());
+
+                    count++;
+                }
+            }.runTaskTimer(SkillMaster.instance, 0, 1);
+        }
+    }
+}
--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/kick/SSKick.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/kick/SSKick.java
@@ -1,61 +1,22 @@
 package net.lifecity.mc.skillmaster.skill.skills.kick;
 
-import net.lifecity.mc.skillmaster.SkillMaster;
-import net.lifecity.mc.skillmaster.skill.Skill;
-import net.lifecity.mc.skillmaster.skill.SkillType;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.weapon.Weapon;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.entity.Entity;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Arrays;
+/**
+ * 敵を蹴り飛ばすスキル
+ */
+public class SSKick extends Kick {
 
-public class SSKick extends Skill {
-
-    public SSKick(SkillUser user) {
+    public SSKick(SkillUser user, int num) {
         super(
-                "蹴り",
                 Weapon.STRAIGHT_SWORD,
-                SkillType.ATTACK,
-                Arrays.asList("相手を蹴り飛ばして、相手と距離を作ります。", "一番近い敵を攻撃します。"),
-                6,
+                num,
                 0,
                 5,
-                user
+                user,
+                1.7,
+                1.8
         );
-    }
-
-    @Override
-    public void activate() {
-
-        double radius = 1.7;
-
-        boolean b = user.attackNearest(
-                radius,
-                1,
-                user.getPlayer().getEyeLocation().getDirection().multiply(1.8),
-                Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK
-        );
-
-        if (b) {
-            Entity entity = user.getNearEntities(radius).get(0);
-            new BukkitRunnable() {
-                int count = 0;
-                @Override
-                public void run() {
-                    if (count >= 12)
-                        cancel();
-                    if (entity.getVelocity().length() <= 0.4)
-                        cancel();
-
-                    particle(Particle.FALLING_DUST, entity.getLocation().add(0, 1, 0), Material.ICE.createBlockData());
-
-                    count++;
-                }
-            }.runTaskTimer(SkillMaster.instance, 0, 1);
-        }
     }
 }
