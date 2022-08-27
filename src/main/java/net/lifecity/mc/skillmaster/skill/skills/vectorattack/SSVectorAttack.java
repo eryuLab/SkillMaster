36bff7ba2501100c2668c9d7370208879f0093e1
Koryu25 <komu.ryu@icloud.com>
2022-08-27 13:11:13 +0900

    [update]: SSVectorAttackにLEを追加

--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/vectorattack/SSVectorAttack.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/vectorattack/SSVectorAttack.java
@@ -1,10 +1,15 @@
 package net.lifecity.mc.skillmaster.skill.skills.vectorattack;
 
+import net.lifecity.mc.skillmaster.SkillMaster;
 import net.lifecity.mc.skillmaster.skill.Skill;
 import net.lifecity.mc.skillmaster.skill.SkillType;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.weapon.Weapon;
+import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.Arrays;
@@ -24,7 +29,7 @@ public class SSVectorAttack extends Skill {
 
         Vector vector = user.getPlayer().getEyeLocation().getDirection().multiply(1.2);
 
-        user.getPlayer().setVelocity(vector);
+        user.getPlayer().setVelocity(user.getPlayer().getVelocity().add(vector));
 
         double damage = user.getPlayer().getVelocity().length();
         damage *= 2;
@@ -38,5 +43,36 @@ public class SSVectorAttack extends Skill {
 
         if (b)
             user.sendMessage("damage: " + damage);
+
+        // 軌道
+        double finalDamage = damage;
+        BlockData data;
+        if (finalDamage > 5)
+            data = Material.RED_CONCRETE_POWDER.createBlockData();
+        else if (finalDamage > 4)
+            data = Material.PURPLE_CONCRETE_POWDER.createBlockData();
+        else if (finalDamage > 3)
+            data = Material.MAGENTA_CONCRETE_POWDER.createBlockData();
+        else if (finalDamage > 2)
+            data = Material.PINK_CONCRETE_POWDER.createBlockData();
+        else
+            data = Material.WHITE_CONCRETE_POWDER.createBlockData();
+
+        new BukkitRunnable() {
+            int count = 0;
+            @Override
+            public void run() {
+                if (count >= 8)
+                    cancel();
+                if (user.getPlayer().getVelocity().length() < 0.47)
+                    cancel();
+                particle(Particle.FALLING_DUST, user.getPlayer().getLocation(), data);
+                if (finalDamage > 5)
+                    particle(Particle.LAVA, user.getPlayer().getLocation());
+                else if (finalDamage > 4 && count % 2 == 1)
+                    particle(Particle.LAVA, user.getPlayer().getLocation());
+                count++;
+            }
+        }.runTaskTimer(SkillMaster.instance, 0, 1);
     }
 }
