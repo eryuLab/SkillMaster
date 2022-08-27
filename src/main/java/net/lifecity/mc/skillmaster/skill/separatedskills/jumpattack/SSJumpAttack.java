36a7e11e69932e1e19b0e370ee294583732aa10b
Koryu25 <komu.ryu@icloud.com>
2022-08-27 14:52:30 +0900

    [update]: SSJumpAttackにLEを追加

--- src/main/java/net/lifecity/mc/skillmaster/skill/separatedskills/jumpattack/SSJumpAttack.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/separatedskills/jumpattack/SSJumpAttack.java
@@ -5,11 +5,15 @@ import net.lifecity.mc.skillmaster.skill.SeparatedSkill;
 import net.lifecity.mc.skillmaster.skill.SkillType;
 import net.lifecity.mc.skillmaster.user.SkillUser;
 import net.lifecity.mc.skillmaster.weapon.Weapon;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 飛び上がり、地面に向かって突撃するスキル
@@ -39,6 +43,29 @@ public class SSJumpAttack extends SeparatedSkill {
 
         user.getPlayer().setVelocity(vector);
 
+        // 煙
+        for (int i = 0; i < 10; i++) {
+            // 座標を生成
+            double max = 0.5;
+            Random random = new Random();
+
+            double x = max * random.nextDouble();
+            if (random.nextBoolean())
+                x *= -1;
+
+            double y = max * random.nextDouble();
+            if (random.nextBoolean())
+                y *= -1;
+
+            double z = max * random.nextDouble();
+            if (random.nextBoolean())
+                z *= -1;
+
+            Location loc = user.getPlayer().getLocation().add(x, y, z);
+
+            particle(Particle.CAMPFIRE_COSY_SMOKE, loc);
+        }
+
         // 指定時間経過後に攻撃入力可能になる
         new BukkitRunnable() {
             @Override
@@ -64,15 +91,39 @@ public class SSJumpAttack extends SeparatedSkill {
 
             step = 2; //段階を2に
 
+            particle(Particle.EXPLOSION_LARGE, user.getPlayer().getEyeLocation());
+
+            new BukkitRunnable() {
+                int count = 0;
+                @Override
+                public void run() {
+                    if (step == 0)
+                        cancel();
+                    if (count >= 10)
+                        cancel();
+                    if (user.getPlayer().getVelocity().length() < 0.3)
+                        cancel();
+
+                    particle(Particle.FALLING_DUST, user.getPlayer().getLocation().add(0, 1, 0), Material.ICE.createBlockData());
+
+                    count++;
+                }
+            }.runTaskTimer(SkillMaster.instance, 0, 1);
+
         } else if (step == 2) {
 
+            double radius = 2;
+
             // 一番近いEntityを攻撃
-            user.attackNearest(
-                    2,
+            boolean b = user.attackNearest(
+                    radius,
                     5,
                     user.getPlayer().getVelocity().setY(0.5),
                     Sound.ENTITY_PLAYER_ATTACK_CRIT
             );
+
+            if (b)
+                particle(Particle.EXPLOSION_LARGE, user.getNearEntities(radius).get(0).getLocation().add(0, 2, 0));
 
             deactivate(); //終了処理
         }
