f15cb74c5d66af96c4e8931bb37c52c6b16392ca
Koryu25 <komu.ryu@icloud.com>
2022-08-31 18:16:48 +0900

    [update]: モードの種類を見直した

--- src/main/java/net/lifecity/mc/skillmaster/user/UserMode.java
+++ src/main/java/net/lifecity/mc/skillmaster/user/UserMode.java
@@ -4,6 +4,13 @@ package net.lifecity.mc.skillmaster.user;
  * SkillUserのモードの列挙
  */
 public enum UserMode {
-    LOBBY,
-    BATTLE
+    BATTLE("バトル"),
+    TRAINING("トレーニング"),
+    UNARMED("武装解除");
+
+    private String jp;
+
+    UserMode(String jp) {
+        this.jp = jp;
+    }
 }
