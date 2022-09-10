36f6878ebaed7079e2c82d5d492a63085778c585
Koryu25 <komu.ryu@icloud.com>
2022-09-10 14:56:25 +0900

    [update]: SkillTypeにアイコンを実装

--- src/main/java/net/lifecity/mc/skillmaster/skill/SkillType.java
+++ src/main/java/net/lifecity/mc/skillmaster/skill/SkillType.java
@@ -1,19 +1,23 @@
 package net.lifecity.mc.skillmaster.skill;
 
 import lombok.Getter;
+import org.bukkit.Material;
 
 /**
  * スキルのおおまかな種類の列挙
  */
 public enum SkillType {
-    ATTACK("攻撃"),
-    DEFENSE("防御"),
-    MOVE("移動");
+    ATTACK("攻撃", Material.STONE_SWORD),
+    DEFENSE("防御", Material.SHIELD),
+    MOVE("移動", Material.CHAINMAIL_BOOTS);
 
     @Getter
     private String jp;
+    @Getter
+    private Material material;
 
-    SkillType(String jp) {
+    SkillType(String jp, Material material) {
         this.jp = jp;
+        this.material = material;
     }
 }
