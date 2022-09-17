8b59d2726a52fb4e0c09478c6b7a0604b277e5a1
Koryu25 <komu.ryu@icloud.com>
2022-09-17 19:04:44 +0900

    [update]: DuelListを追加

--- src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
+++ src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
@@ -2,6 +2,7 @@ package net.lifecity.mc.skillmaster;
 
 import dev.jorel.commandapi.CommandAPI;
 import lombok.Getter;
+import net.lifecity.mc.skillmaster.game.DuelList;
 import net.lifecity.mc.skillmaster.user.SkillUserList;
 import net.lifecity.mc.skillmaster.utils.FileUtil;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,6 +14,9 @@ public final class SkillMaster extends JavaPlugin {
     @Getter
     private SkillUserList userList;
 
+    @Getter
+    private DuelList duelList;
+
     @Override
     public void onEnable() {
         instance = this;
@@ -22,6 +26,8 @@ public final class SkillMaster extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
 
         userList = new SkillUserList();
+
+        duelList = new DuelList();
 
         FileUtil fileUtils = new FileUtil();
         fileUtils.init();
