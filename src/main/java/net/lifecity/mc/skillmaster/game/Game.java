f073c19115eacedeb2ffa7a05cb168961dcef5dd
Koryu25 <komu.ryu@icloud.com>
2022-09-19 02:29:53 +0900

    [update]: タイトル送信メソッドの追加

--- src/main/java/net/lifecity/mc/skillmaster/game/Game.java
+++ src/main/java/net/lifecity/mc/skillmaster/game/Game.java
@@ -1,7 +1,12 @@
 package net.lifecity.mc.skillmaster.game;
 
 import net.lifecity.mc.skillmaster.user.SkillUser;
+import org.bukkit.scheduler.BukkitRunnable;
 
+/**
+ * ひとつのゲームを管理します
+ * フィールドとチームは継承先で実装してください
+ */
 public abstract class Game {
 
     protected final GameType type; //ゲームのタイプ
@@ -14,7 +19,8 @@ public abstract class Game {
     /**
      * ゲームのインスタンスを生成します
      * @param type ゲームのタイプ
-     * @param time ゲームの時間
+     * @param time ゲームの時間(秒)
+     * @param countDownTime ゲーム開始前のカウントダウンの時間(秒)
      */
     protected Game(GameType type, int time, int countDownTime) {
         this.type = type;
@@ -25,16 +31,34 @@ public abstract class Game {
     public void start() {
         // テレポート
         teleportAll();
+
         // カウントダウン
+
         // ゲーム状態移行
+        state = GameState.IN_GAMING;
+
         // タイマースタート
     }
 
     public void stop() {
         // ゲーム状態移行
+        state = GameState.WAITING_FOR_FINISH;
+
         // 勝敗表示
+
         // ロビーへ接続
     }
+
+    /**
+     * ゲーム内のすべてのプレイヤーにタイトルを送信します
+     */
+    public abstract void sendTitleAll();
+
+    /**
+     * 指定チームにタイトルを送信します
+     * @param team 指定チーム
+     */
+    public abstract void sendTitleTeam(GameTeam team);
 
     /**
      * ゲーム内すべてのプレイヤーを初期地点にテレポートします
@@ -50,4 +74,17 @@ public abstract class Game {
      * ゲーム内の特定のプレイヤーを初期地点にテレポートします
      */
     public abstract void teleportOne(SkillUser target);
+
+    private class CountDownTimer extends BukkitRunnable {
+
+        private int count = 0;
+
+        @Override
+        public void run() {
+            // ゲームの状態がカウントダウン中でなかったらタスクキャンセル
+            if (state != GameState.COUNT_DOWN)
+                cancel();
+            // タイトル表示
+        }
+    }
 }
