f0ad45287542c2d65ac966f4d55eccd71d08514a
Koryu25 <komu.ryu@icloud.com>
2022-09-19 03:53:06 +0900

    [update]: 継承メソッドを実装

--- src/main/java/net/lifecity/mc/skillmaster/game/oneonone/OneOnOne.java
+++ src/main/java/net/lifecity/mc/skillmaster/game/oneonone/OneOnOne.java
@@ -1,6 +1,7 @@
 package net.lifecity.mc.skillmaster.game.oneonone;
 
 import net.lifecity.mc.skillmaster.game.Game;
+import net.lifecity.mc.skillmaster.game.GameResult;
 import net.lifecity.mc.skillmaster.game.GameTeam;
 import net.lifecity.mc.skillmaster.game.GameType;
 import net.lifecity.mc.skillmaster.user.SkillUser;
@@ -20,15 +21,27 @@ public class OneOnOne extends Game {
         this.teamB = new GameTeam("Beta", ChatColor.BLUE, new SkillUser[]{userB});
     }
 
-
     @Override
-    public void sendTitleAll() {
+    public void sendResult() {
+        // 結果集計
+        String sub = "A:33 --- B:4";
 
+        GameTeam winner = teamA;
+        GameTeam loser = teamB;
+
+        // タイトル送信
+        winner.sendTitle(GameResult.WIN.getEn(), sub);
+        loser.sendTitle(GameResult.LOSE.getEn(), sub);
     }
 
     @Override
-    public void sendTitleTeam(GameTeam team) {
+    public boolean hasTeam(GameTeam team) {
+        return team == teamA || team == teamB;
+    }
 
+    @Override
+    public GameTeam[] getTeams() {
+        return new GameTeam[]{teamA, teamB};
     }
 
     @Override
@@ -43,13 +56,5 @@ public class OneOnOne extends Game {
             team.teleportAll(field.spawnPointA);
         else if (team == teamB)
             team.teleportAll(field.spawnPointB);
-    }
-
-    @Override
-    public void teleportOne(SkillUser target) {
-        if (teamA.belongs(target))
-            target.getPlayer().teleport(field.spawnPointA);
-        else if (teamB.belongs(target))
-            target.getPlayer().teleport(field.spawnPointB);
     }
 }
