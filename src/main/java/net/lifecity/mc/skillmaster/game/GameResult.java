290e51d1eb141117de4764fcac47b11570f50180
Koryu25 <komu.ryu@icloud.com>
2022-09-19 03:52:44 +0900

    [update]: 英語ラベルの追加

--- src/main/java/net/lifecity/mc/skillmaster/game/GameResult.java
+++ src/main/java/net/lifecity/mc/skillmaster/game/GameResult.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum GameResult {
-    WIN("勝利"),
-    LOSE("敗北"),
-    DRAW("引き分け");
+    WIN("勝利", "Win"),
+    LOSE("敗北", "Lose"),
+    DRAW("引き分け", "Draw");
 
     @Getter
     private String jp;
+    @Getter
+    private String en;
 }
