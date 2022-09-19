47ff2b0299e566fde3222302f9d257552c4843f7
Koryu25 <komu.ryu@icloud.com>
2022-09-19 19:14:08 +0900

    [update]: gameFieldListの追加とテストデータの挿入

--- src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
+++ src/main/java/net/lifecity/mc/skillmaster/SkillMaster.java
@@ -2,9 +2,14 @@ package net.lifecity.mc.skillmaster;
 
 import dev.jorel.commandapi.CommandAPI;
 import lombok.Getter;
+import net.lifecity.mc.skillmaster.game.GameFieldList;
 import net.lifecity.mc.skillmaster.game.GameList;
+import net.lifecity.mc.skillmaster.game.GameType;
+import net.lifecity.mc.skillmaster.game.field.TwoPointField;
 import net.lifecity.mc.skillmaster.user.SkillUserList;
 import net.lifecity.mc.skillmaster.utils.FileUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkillMaster extends JavaPlugin {
@@ -13,6 +18,9 @@ public final class SkillMaster extends JavaPlugin {
 
     @Getter
     private SkillUserList userList;
+
+    @Getter
+    private GameFieldList gameFieldList;
 
     @Getter
     private GameList gameList;
@@ -26,6 +34,14 @@ public final class SkillMaster extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
 
         userList = new SkillUserList();
+
+        gameFieldList = new GameFieldList();
+        gameFieldList.add(new TwoPointField(
+                "闘技場",
+                new GameType[]{GameType.DUEL, GameType.ONE_ON_ONE},
+                new Location(Bukkit.getWorlds().get(0), 123, -25, -18),
+                new Location(Bukkit.getWorlds().get(0), 123, -25, -18))
+        );
 
         gameList = new GameList();
 
