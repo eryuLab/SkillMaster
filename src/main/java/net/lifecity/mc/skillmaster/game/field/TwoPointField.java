dbce0722cd323026f572de2abde67fb7f6ac4d15
Koryu25 <komu.ryu@icloud.com>
2022-09-19 16:52:53 +0900

    [update]: 引数の渡し方を変更

--- src/main/java/net/lifecity/mc/skillmaster/game/field/TwoPointField.java
+++ src/main/java/net/lifecity/mc/skillmaster/game/field/TwoPointField.java
@@ -12,8 +12,8 @@ public class TwoPointField extends GameField {
     @Getter
     protected final Location pointB;
 
-    public TwoPointField(String name, GameType type, Location pointA, Location pointB) {
-        super(name, type);
+    public TwoPointField(String name, GameType[] typeArray, Location pointA, Location pointB) {
+        super(name, typeArray);
         this.pointA = pointA;
         this.pointB = pointB;
     }
