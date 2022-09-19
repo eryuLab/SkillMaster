320b44545916edd6343bb06e6eeeb4e01b719076
Koryu25 <komu.ryu@icloud.com>
2022-09-19 18:36:34 +0900

    [update]: Dataを削除

--- src/main/java/net/lifecity/mc/skillmaster/game/GameResult.java
+++ src/main/java/net/lifecity/mc/skillmaster/game/GameResult.java
@@ -3,9 +3,6 @@ package net.lifecity.mc.skillmaster.game;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @AllArgsConstructor
 public enum GameResult {
     WIN("勝利", "Win"),
@@ -16,14 +13,4 @@ public enum GameResult {
     private String jp;
     @Getter
     private String en;
-
-    public class Data {
-
-        @Getter
-        private final Set<GameTeam> winner = new HashSet<>();
-        @Getter
-        private final Set<GameTeam> loser = new HashSet<>();
-        @Getter
-        private final Set<GameTeam> drawer = new HashSet<>();
-    }
 }
