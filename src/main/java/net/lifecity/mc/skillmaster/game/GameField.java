f4426bd60cf75847622af3bbf282570056863264
Koryu25 <komu.ryu@icloud.com>
2022-09-19 16:38:36 +0900

    [update]: フィールドが複数のゲームタイプを保持できるように変更

--- src/main/java/net/lifecity/mc/skillmaster/game/GameField.java
+++ src/main/java/net/lifecity/mc/skillmaster/game/GameField.java
@@ -3,6 +3,10 @@ package net.lifecity.mc.skillmaster.game;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * ゲームに使う場所を管理します
  * スポーンやギミックに使うLocationは継承先のフィールドとして実装してください
@@ -10,14 +14,23 @@ import lombok.Setter;
 public abstract class GameField {
 
     protected final String name;
-    protected final GameType type;
+    protected final Set<GameType> typeList = new HashSet<>();
 
     @Getter
     @Setter
     protected Game nowGame = null;// 稼働中のゲーム
 
-    protected GameField(String name, GameType type) {
+    protected GameField(String name, GameType... typeArray) {
         this.name = name;
-        this.type = type;
+        this.typeList.addAll(List.of(typeArray));
+    }
+
+    /**
+     * 引数のゲームタイプとして使えるかを判定します
+     * @param type ゲームタイプ
+     * @return 使えるとtrue
+     */
+    public boolean usableAs(GameType type) {
+        return typeList.contains(type);
     }
 }
