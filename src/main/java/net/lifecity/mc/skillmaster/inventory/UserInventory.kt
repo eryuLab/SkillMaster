78bd137b1633809031bd28a399091e3f4d61f30e
hirotask <hirotaskhashi@gmail.com>
2022-09-23 23:17:37 +0900

    [update]: UserInventoryをKotlinに変換

--- src/main/java/net/lifecity/mc/skillmaster/inventory/UserInventory.kt
+++ src/main/java/net/lifecity/mc/skillmaster/inventory/UserInventory.kt
@@ -1,61 +1,55 @@
-package net.lifecity.mc.skillmaster.inventory;
+package net.lifecity.mc.skillmaster.inventory
 
-import net.lifecity.mc.skillmaster.SkillMaster;
-import net.lifecity.mc.skillmaster.skill.Skill;
-import net.lifecity.mc.skillmaster.skill.SkillManager;
-import net.lifecity.mc.skillmaster.user.SkillUser;
-import net.lifecity.mc.skillmaster.user.skillset.SkillKey;
-import org.bukkit.Material;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.List;
+import net.lifecity.mc.skillmaster.SkillMaster
+import net.lifecity.mc.skillmaster.skill.SkillManager
+import net.lifecity.mc.skillmaster.user.SkillUser
+import net.lifecity.mc.skillmaster.user.skillset.SkillButton
+import net.lifecity.mc.skillmaster.user.skillset.SkillKey
+import org.bukkit.Material
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.scheduler.BukkitRunnable
+import java.util.List
+import java.util.function.Consumer
 
 /**
  * SkillMasterのユーザーのインベントリを構築、操作するためのクラス
  */
-public class UserInventory extends InventoryFrame {
-
-    public UserInventory(SkillUser user) {
-        super(user);
-    }
-
-    @Override
-    public void init() {
+class UserInventory(user: SkillUser?) : InventoryFrame(user!!) {
+    override fun init() {
         // アイテムを設置
         // 右クリック
-        for (SkillKey key : user.getRightSkillSet()) {
-            setSkillItem(key);
+        for (key in user.rightSkillSet) {
+            setSkillItem(key)
         }
         // スワップキー
-        for (SkillKey key : user.getSwapSkillSet()) {
-            setSkillItem(key);
+        for (key in user.swapSkillSet) {
+            setSkillItem(key)
         }
         // ドロップキー
-        for (SkillKey key : user.getDropSkillSet()) {
-            setSkillItem(key);
+        for (key in user.dropSkillSet) {
+            setSkillItem(key)
         }
-
     }
 
     /**
      * スキルと説明アイテムをセットで追加します
      * @param key 追加するSkillKey
      */
-    private void setSkillItem(SkillKey key) {
+    private fun setSkillItem(key: SkillKey) {
         // 配置するスロットを計算
-        int slot = switch (key.getButton()) {
-            case DROP -> 10;
-            case SWAP -> 13;
-            case RIGHT -> 16;
-        };
-        slot = slot + 9 * key.getNum();
+        var slot = when (key.button) {
+            SkillButton.DROP -> 10
+            SkillButton.SWAP -> 13
+            SkillButton.RIGHT -> 16
+        }
+        slot = slot + 9 * key.num
 
         // Paneを設置
-        setItem(slot - 1, paneItem(key, true));
-        setItem(slot + 1, paneItem(key, false));
+        setItem(slot - 1, paneItem(key, true))
+        setItem(slot + 1, paneItem(key, false))
         // SkillItemを設置
-        setItem(slot, skillItem(key));
+        setItem(slot, skillItem(key))
     }
 
     /**
@@ -63,27 +57,21 @@ public class UserInventory extends InventoryFrame {
      * @param key SkillKeyで生成するアイテムを選択します
      * @return 生成されたスキル配置を説明するアイテム
      */
-    private InvItem paneItem(SkillKey key, boolean isLeft) {
-        Material material = switch (key.getButton()) {
-            case RIGHT -> Material.YELLOW_STAINED_GLASS_PANE;
-            case SWAP -> Material.LIGHT_BLUE_STAINED_GLASS_PANE;
-            case DROP -> Material.PINK_STAINED_GLASS_PANE;
-        };
-
-        String name;
-        if (isLeft)
-            name = key.getButton().getJp() + ": " + key.getNum() + "→";
-        else
-            name = "←" + key.getButton().getJp() + ": " + key.getNum();
-
-        return new InvItem(
-                createItemStack(
-                        material,
-                        name,
-                        List.of()
-                ),
-                event -> event.setCancelled(true)
-        );
+    private fun paneItem(key: SkillKey, isLeft: Boolean): InvItem {
+        val material = when (key.button) {
+            SkillButton.RIGHT -> Material.YELLOW_STAINED_GLASS_PANE
+            SkillButton.SWAP -> Material.LIGHT_BLUE_STAINED_GLASS_PANE
+            SkillButton.DROP -> Material.PINK_STAINED_GLASS_PANE
+        }
+        val name: String
+        name = if (isLeft) key.button.jp + ": " + key.num + "→" else "←" + key.button.jp + ": " + key.num
+        return InvItem(
+            createItemStack(
+                material,
+                name,
+                List.of()
+            )
+        ) { event: InventoryClickEvent -> event.isCancelled = true }
     }
 
     /**
@@ -91,104 +79,86 @@ public class UserInventory extends InventoryFrame {
      * @param key 変換するSkillKey
      * @return 変換されたSkillKey
      */
-    private InvItem skillItem(SkillKey key) {
+    private fun skillItem(key: SkillKey): InvItem {
 
         // スキルアイテムがない
-        if (key.getSkill() == null) {
-            return new InvItem(
-                    createItemStack(Material.IRON_BARS, "スキル未登録", List.of()),
-                    event -> {
-                        // スキルアイテムを置くとスキル変更
+        return if (key.skill == null) {
+            InvItem(
+                createItemStack(Material.IRON_BARS, "スキル未登録", List.of()),
+                Consumer { event: InventoryClickEvent ->
+                    // スキルアイテムを置くとスキル変更
 
-                        // インベントリ確認
-                        if (event.getView().getTopInventory().getType() != InventoryType.CHEST) {
-                            event.setCancelled(true);
-                            return;
-                        }
-
-                        // インベントリ特定
-                        if (event.getView().getTopInventory().getType() == InventoryType.CHEST) {
-
-                            InventoryFrame openedInv = user.getOpenedInventory();
-
-                            if (openedInv == null) {
-                                event.setCancelled(true);
-                                return;
-                            }
-
-                            if (event.getView().getTopInventory() == openedInv.inv) {
-
-                                // カーソルがスキルであればスキルを登録
-                                Skill cursorSkill = new SkillManager(user).fromItemStack(event.getCursor());
-
-                                // カーソルがスキルであるか
-                                if (cursorSkill == null) {
-                                    event.setCancelled(true);
-                                    return;
-                                }
-
-                                // セットできるか確認
-                                if (!user.settable(cursorSkill)) {
-                                    event.setCancelled(true);
-                                    user.getPlayer().sendMessage("このスキルはすでに登録されています");
-                                    return;
-                                }
-
-                                // スキルセット
-                                key.setSkill(cursorSkill);
-                                user.getPlayer().sendMessage("スキルを登録しました: " + key.getButton().getJp() + "[" + key.getNum() + "]: " + cursorSkill.getName());
-
-                                setItem(event.getSlot(), skillItem(key));
-                                new BukkitRunnable() {
-                                    @Override
-                                    public void run() {
-                                        user.getUserInventory().inv.getItem(event.getSlot()).setAmount(1);
-                                    }
-                                }.runTaskLater(SkillMaster.instance, 1);
-                            }
-                        }
+                    // インベントリ確認
+                    if (event.view.topInventory.type != InventoryType.CHEST) {
+                        event.isCancelled = true
                     }
 
-            );
-        }
-        // スキルアイテムがある
-        else {
-            return new InvItem(
-                    key.getSkill().toItemStack(),
-                    event -> {
-                        // スキルアイテム除外
-
-                        if (event.getView().getTopInventory().getType() != InventoryType.CHEST) {
-                            event.setCancelled(true);
-                            return;
+                    // インベントリ特定
+                    if (event.view.topInventory.type == InventoryType.CHEST) {
+                        val openedInv = user.openedInventory
+                        if (openedInv == null) {
+                            event.isCancelled = true
                         }
+                        if (event.view.topInventory === openedInv!!.inv) {
 
-                        // インベントリ特定
-                        if (event.getView().getTopInventory().getType() == InventoryType.CHEST) {
-                            InventoryFrame openedInv = user.getOpenedInventory();
+                            // カーソルがスキルであればスキルを登録
+                            val cursorSkill = SkillManager(user).fromItemStack(event.cursor!!)
 
-                            if (openedInv == null) {
-                                event.setCancelled(true);
-                                return;
+                            // カーソルがスキルであるか
+                            if (cursorSkill == null) {
+                                event.isCancelled = true
                             }
 
-                            if (event.getView().getTopInventory() == openedInv.inv) {
-
-                                // スキル除外
-                                key.setSkill(null);
-                                user.getPlayer().sendMessage("スキルを除外しました");
-                                setItem(event.getSlot(), skillItem(key));
-                                new BukkitRunnable() {
-                                    @Override
-                                    public void run() {
-                                        user.getUserInventory().inv.getItem(event.getSlot()).setAmount(1);
-                                    }
-                                }.runTaskLater(SkillMaster.instance, 1);
-                                event.setCancelled(true);
+                            // セットできるか確認
+                            if (!user.settable(cursorSkill!!)) {
+                                event.isCancelled = true
+                                user.player.sendMessage("このスキルはすでに登録されています")
                             }
+
+                            // スキルセット
+                            key.skill = cursorSkill
+                            user.player.sendMessage("スキルを登録しました: " + key.button.jp + "[" + key.num + "]: " + cursorSkill.name)
+                            setItem(event.slot, skillItem(key))
+                            object : BukkitRunnable() {
+                                override fun run() {
+                                    user.userInventory.inv.getItem(event.slot)!!.amount = 1
+                                }
+                            }.runTaskLater(SkillMaster.instance, 1)
                         }
                     }
-            );
+                }
+            )
+        } else {
+            InvItem(
+                key.skill!!.toItemStack(),
+                Consumer { event: InventoryClickEvent ->
+                    // スキルアイテム除外
+                    if (event.view.topInventory.type != InventoryType.CHEST) {
+                        event.isCancelled = true
+                    }
+
+                    // インベントリ特定
+                    if (event.view.topInventory.type == InventoryType.CHEST) {
+                        val openedInv = user.openedInventory
+                        if (openedInv == null) {
+                            event.isCancelled = true
+                        }
+                        if (event.view.topInventory === openedInv!!.inv) {
+
+                            // スキル除外
+                            key.skill = null
+                            user.player.sendMessage("スキルを除外しました")
+                            setItem(event.slot, skillItem(key))
+                            object : BukkitRunnable() {
+                                override fun run() {
+                                    user.userInventory.inv.getItem(event.slot)!!.amount = 1
+                                }
+                            }.runTaskLater(SkillMaster.instance, 1)
+                            event.isCancelled = true
+                        }
+                    }
+                }
+            )
         }
     }
 }
