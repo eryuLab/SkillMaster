d32974c4f99a446a4142671855e56287f0d8fe8a
hirotask <hirotaskhashi@gmail.com>
2022-09-23 15:11:43 +0900

    [update]: VectorAttackをKotlinに変換

--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/VectorAttack.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/VectorAttack.kt
@@ -1,83 +1,62 @@
-package net.lifecity.mc.skillmaster.skill.skills;
+package net.lifecity.mc.skillmaster.skill.skills
 
-import net.lifecity.mc.skillmaster.SkillMaster;
-import net.lifecity.mc.skillmaster.skill.Skill;
-import net.lifecity.mc.skillmaster.skill.SkillType;
-import net.lifecity.mc.skillmaster.user.SkillUser;
-import net.lifecity.mc.skillmaster.weapon.Weapon;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
+import com.github.syari.spigot.api.particle.spawnParticle
+import com.github.syari.spigot.api.scheduler.runTaskTimer
+import net.lifecity.mc.skillmaster.SkillMaster
+import net.lifecity.mc.skillmaster.skill.Skill
+import net.lifecity.mc.skillmaster.skill.SkillType
+import net.lifecity.mc.skillmaster.user.SkillUser
+import net.lifecity.mc.skillmaster.weapon.Weapon
+import org.bukkit.Material
+import org.bukkit.Particle
+import org.bukkit.Sound
+import org.bukkit.block.data.BlockData
 
-import java.util.Arrays;
+class VectorAttack(user: SkillUser?) : Skill(
+    "ベクトルアタック",
+    listOf(Weapon.STRAIGHT_SWORD, Weapon.GREAT_SWORD, Weapon.LONG_SWORD, Weapon.MACE),
+    SkillType.ATTACK,
+    listOf("ユーザーが持つベクトルを力に変換して攻撃します。"),
+    0,
+    40,
+    user
+) {
+    override fun activate() {
+        super.activate()
 
-public class VectorAttack extends Skill {
+        val vector = user.player.eyeLocation.direction.multiply(1.2)
+        user.player.velocity = user.player.velocity.add(vector)
 
-    public VectorAttack(SkillUser user) {
-        super(
-                "ベクトルアタック",
-                Arrays.asList(Weapon.STRAIGHT_SWORD, Weapon.GREAT_SWORD, Weapon.LONG_SWORD, Weapon.MACE),
-                SkillType.ATTACK,
-                Arrays.asList("ユーザーが持つベクトルを力に変換して攻撃します。"),
-                0,
-                40,
-                user
-        );
-    }
+        var damage = user.player.velocity.length()
+        damage *= 2.0
 
-    @Override
-    public void activate() {
-        super.activate();
-
-        Vector vector = user.getPlayer().getEyeLocation().getDirection().multiply(1.2);
-
-        user.getPlayer().setVelocity(user.getPlayer().getVelocity().add(vector));
-
-        double damage = user.getPlayer().getVelocity().length();
-        damage *= 2;
-
-        boolean b = user.attackNearest(
-                1.8,
-                damage,
-                user.getPlayer().getVelocity().multiply(0.25).setY(0.15),
-                Sound.ENTITY_PLAYER_ATTACK_CRIT
-        );
-
-        if (b)
-            user.sendMessage("damage: " + damage);
+        val b = user.attackNearest(
+            1.8,
+            damage,
+            user.player.velocity.multiply(0.25).setY(0.15),
+            Sound.ENTITY_PLAYER_ATTACK_CRIT
+        )
+        if (b) user.sendMessage("damage: $damage")
 
         // 軌道
-        double finalDamage = damage;
-        BlockData data;
-        if (finalDamage > 5)
-            data = Material.RED_CONCRETE_POWDER.createBlockData();
-        else if (finalDamage > 4)
-            data = Material.PURPLE_CONCRETE_POWDER.createBlockData();
-        else if (finalDamage > 3)
-            data = Material.MAGENTA_CONCRETE_POWDER.createBlockData();
-        else if (finalDamage > 2)
-            data = Material.PINK_CONCRETE_POWDER.createBlockData();
-        else
-            data = Material.WHITE_CONCRETE_POWDER.createBlockData();
+        val data: BlockData =
+            if (damage > 5) Material.RED_CONCRETE_POWDER.createBlockData()
+            else if (damage > 4) Material.PURPLE_CONCRETE_POWDER.createBlockData()
+            else if (damage > 3) Material.MAGENTA_CONCRETE_POWDER.createBlockData()
+            else if (damage > 2) Material.PINK_CONCRETE_POWDER.createBlockData()
+            else Material.WHITE_CONCRETE_POWDER.createBlockData()
 
-        new BukkitRunnable() {
-            int count = 0;
-            @Override
-            public void run() {
-                if (count >= 8)
-                    cancel();
-                if (user.getPlayer().getVelocity().length() < 0.47)
-                    cancel();
-                particle(Particle.FALLING_DUST, user.getPlayer().getLocation(), data);
-                if (finalDamage > 5)
-                    particle(Particle.LAVA, user.getPlayer().getLocation());
-                else if (finalDamage > 4 && count % 2 == 1)
-                    particle(Particle.LAVA, user.getPlayer().getLocation());
-                count++;
-            }
-        }.runTaskTimer(SkillMaster.instance, 0, 1);
+        var count = 0
+        SkillMaster.instance.runTaskTimer(1) {
+            if (count >= 8) cancel()
+            if (user.player.velocity.length() < 0.47) cancel()
+
+            val loc = user.player.location
+            loc.spawnParticle(Particle.FALLING_DUST, data = data)
+
+            if (damage > 5) loc.spawnParticle(Particle.LAVA)
+            else if (damage > 4 && count % 2 == 1) loc.spawnParticle(Particle.LAVA)
+            count++
+        }
     }
 }
