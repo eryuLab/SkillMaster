6c6f363bb36c8bc13fbeb26658cc4a53970a70ae
hirotask <hirotaskhashi@gmail.com>
2022-09-23 15:41:45 +0900

    [update]: SkillTypeをKotlinに変換

--- src/main/java/net/lifecity/mc/skillmaster/skill/SkillType.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/SkillType.kt
@@ -1,20 +1,10 @@
-package net.lifecity.mc.skillmaster.skill;
+package net.lifecity.mc.skillmaster.skill
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.bukkit.Material;
+import org.bukkit.Material
 
 /**
  * スキルのおおまかな種類の列挙
  */
-@AllArgsConstructor
-public enum SkillType {
-    ATTACK("攻撃", Material.STONE_SWORD),
-    DEFENSE("防御", Material.SHIELD),
-    MOVE("移動", Material.CHAINMAIL_BOOTS);
-
-    @Getter
-    private final String jp;
-    @Getter
-    private final Material material;
+enum class SkillType(val jp : String, val material : Material) {
+    ATTACK("攻撃", Material.STONE_SWORD), DEFENSE("防御", Material.SHIELD), MOVE("移動", Material.CHAINMAIL_BOOTS);
 }
