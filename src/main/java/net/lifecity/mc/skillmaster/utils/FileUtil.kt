cfa9ca77cd7d68d327731810d9cc15baed442ffe
hirotask <hirotaskhashi@gmail.com>
2022-09-25 00:53:32 +0900

    [update]: FileUtilをKotlinに変換

--- src/main/java/net/lifecity/mc/skillmaster/utils/FileUtil.kt
+++ src/main/java/net/lifecity/mc/skillmaster/utils/FileUtil.kt
@@ -1,42 +1,34 @@
-package net.lifecity.mc.skillmaster.utils;
+package net.lifecity.mc.skillmaster.utils
 
-import net.lifecity.mc.skillmaster.SkillMaster;
-import org.apache.commons.io.FileUtils;
+import net.lifecity.mc.skillmaster.SkillMaster
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+class FileUtil {
+    private val files = arrayOf(
+        "east_wall.schem",
+        "north_east_wall.schem",
+        "north_wall.schem",
+        "north_west_wall.schem",
+        "south_east_wall.schem",
+        "south_wall.schem",
+        "south_west_wall.schem",
+        "west_wall.schem"
+    )
 
-public class FileUtil {
-
-    private final String[] files = {
-            "east_wall.schem",
-            "north_east_wall.schem",
-            "north_wall.schem",
-            "north_west_wall.schem",
-            "south_east_wall.schem",
-            "south_wall.schem",
-            "south_west_wall.schem",
-            "west_wall.schem"
-    };
-
-    public void init() {
-        SkillMaster skillMaster = SkillMaster.instance;
-
-        for(String fileName : files) {
-            InputStream stream = skillMaster.getResource("schematics/" + fileName);
-
-            if (stream != null)
-            {
-                File file = new File(skillMaster.getDataFolder(), "schematics/" + fileName);
+    fun init() {
+        val skillMaster = SkillMaster.instance
+        for (fileName in files) {
+            val stream = skillMaster.getResource("schematics/$fileName")
+            if (stream != null) {
+                val file = File(skillMaster.dataFolder, "schematics/$fileName")
                 try {
-                    FileUtils.copyInputStreamToFile(stream,file);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    FileUtils.copyInputStreamToFile(stream, file)
+                } catch (e: IOException) {
+                    e.printStackTrace()
                 }
             }
         }
-
     }
 }
