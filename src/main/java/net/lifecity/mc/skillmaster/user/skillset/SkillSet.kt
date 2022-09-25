c8320e2f58b31887995b98046345ecda00e9b644
Koryu25 <komu.ryu@icloud.com>
2022-09-26 08:59:40 +0900

    [update]: null化不可に変更

--- src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
@@ -2,7 +2,7 @@ package net.lifecity.mc.skillmaster.user.skillset
 
 data class SkillSet(
     val button: SkillButton,
-    var indexOne: SkillKey?,
-    var indexTwo: SkillKey?,
-    var indexThree: SkillKey?
+    var indexOne: SkillKey,
+    var indexTwo: SkillKey,
+    var indexThree: SkillKey
 )
