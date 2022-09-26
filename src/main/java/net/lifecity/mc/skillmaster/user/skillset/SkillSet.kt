bc67a61c14a603b5da58e2e980c4993bbd817c25
Koryu25 <komu.ryu@icloud.com>
2022-09-26 09:51:07 +0900

    [update]: 変数名を修正

--- src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
@@ -2,7 +2,7 @@ package net.lifecity.mc.skillmaster.user.skillset
 
 data class SkillSet(
     val button: SkillButton,
-    val list: List<SkillKey> = listOf(
+    val keyList: List<SkillKey> = listOf(
         SkillKey(button, 0, null),
         SkillKey(button, 1, null),
         SkillKey(button, 2, null)
