9bae18d009172ea7433dd7c39bc1cfb25d5b2b77
Koryu25 <komu.ryu@icloud.com>
2022-09-26 09:49:18 +0900

    [update]: 3つのSkillKeyをリストに入れた

--- src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
@@ -2,7 +2,9 @@ package net.lifecity.mc.skillmaster.user.skillset
 
 data class SkillSet(
     val button: SkillButton,
-    val indexOne: SkillKey = SkillKey(button, 1, null),
-    val indexTwo: SkillKey = SkillKey(button, 2, null),
-    val indexThree: SkillKey = SkillKey(button, 3, null)
+    val list: List<SkillKey> = listOf(
+        SkillKey(button, 0, null),
+        SkillKey(button, 1, null),
+        SkillKey(button, 2, null)
+    )
 )
