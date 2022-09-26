a7e1f3ed962d1faad04d39ffe1db4b69d6cb0ad4
Koryu25 <komu.ryu@icloud.com>
2022-09-26 11:13:24 +0900

    [update]: インデックスが一定値に達したらリセットされるようにセッタを定義

--- src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillCard.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillCard.kt
@@ -1,7 +1,19 @@
 package net.lifecity.mc.skillmaster.user.skillset
 
+import net.lifecity.mc.skillmaster.skill.Skill
+
 data class SkillCard(
     val button: SkillButton,
-    val skillSet: SkillSet = SkillSet(button),
+    val skillSet: SkillSet = SkillSet(button)
+) {
     var index: Int = 0
-)
+    set(value) {
+        if (value == skillSet.keyList.size)
+            field = 0
+        field = value
+    }
+
+    fun now(): Skill? {
+        return skillSet.keyList[index].skill
+    }
+}
