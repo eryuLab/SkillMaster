08336d18126e73c07a12f3860af3b17ee6848f30
Koryu25 <komu.ryu@icloud.com>
2022-09-26 09:05:26 +0900

    [update]: 3つのSkillKeyフィールドの定数化と初期化

--- src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
@@ -2,7 +2,7 @@ package net.lifecity.mc.skillmaster.user.skillset
 
 data class SkillSet(
     val button: SkillButton,
-    var indexOne: SkillKey,
-    var indexTwo: SkillKey,
-    var indexThree: SkillKey
+    val indexOne: SkillKey = SkillKey(button, 1, null),
+    val indexTwo: SkillKey = SkillKey(button, 2, null),
+    val indexThree: SkillKey = SkillKey(button, 3, null)
 )
