3e812628acf33ace5604a917cf24a0cdc13ff853
hirotask <hirotaskhashi@gmail.com>
2022-09-29 23:18:30 +0900

    [update]: GameStageのKotlin化

--- src/main/java/net/lifecity/mc/skillmaster/game/stage/GameStage.kt
+++ src/main/java/net/lifecity/mc/skillmaster/game/stage/GameStage.kt
@@ -1,37 +1,22 @@
-package net.lifecity.mc.skillmaster.game.stage;
+package net.lifecity.mc.skillmaster.game.stage
 
-import lombok.Getter;
-import lombok.Setter;
-import net.lifecity.mc.skillmaster.game.Game;
-import net.lifecity.mc.skillmaster.game.stage.field.TwoPoint;
-
-import java.util.*;
+import net.lifecity.mc.skillmaster.game.Game
 
 /**
  * ゲームに使う場所を管理します
  * スポーンやギミックに使うLocationは継承先のフィールドとして実装してください
  */
-public class GameStage {
+class GameStage(val name: String) {
+    val fieldMap = mutableMapOf<FieldType, GameField>()
 
-    @Getter
-    private final String name;
-    @Getter
-    private final Map<FieldType, GameField> fieldMap = new HashMap<>();
-
-    @Getter
-    @Setter
-    private Game nowGame = null;// 稼働中のゲーム
-
-    public GameStage(String name) {
-        this.name = name;
-    }
+    var nowGame: Game? = null // 稼働中のゲーム
 
     /**
      * フィールドを追加します
      * @param field フィールド
      */
-    public void addField(GameField field) {
-        fieldMap.put(field.type, field);
+    fun addField(field: GameField) {
+        fieldMap[field.type] = field
     }
 
     /**
@@ -39,25 +24,23 @@ public class GameStage {
      * @param type 指定タイプ
      * @return フィールド
      */
-    public GameField getField(FieldType type) {
-        return fieldMap.get(type);
-    }
+    fun getField(type: FieldType) = fieldMap[type]
+
 
     /**
      * 引数のゲームタイプとして使えるかを判定します
      * @param type ゲームタイプ
      * @return 使えるとtrue
      */
-    public boolean usableAs(FieldType type) {
-        GameField field = fieldMap.get(type);
-        return field != null;
+    fun usableAs(type: FieldType): Boolean {
+        val field = fieldMap[type]
+        return field != null
     }
 
     /**
      * 現在このフィールドが使用されているか判定します
      * @return 使われていたらtrue
      */
-    public boolean inUsing() {
-        return nowGame != null;
-    }
+    fun inUsing() = nowGame != null
+
 }
