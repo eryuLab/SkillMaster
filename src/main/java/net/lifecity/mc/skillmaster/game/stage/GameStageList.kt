da509dcaf9923f41b99f78b65dbb7eb0a9add1dc
hirotask <hirotaskhashi@gmail.com>
2022-09-29 23:25:39 +0900

    [update]: GameStageListのKotlin化

---
+++ src/main/java/net/lifecity/mc/skillmaster/game/stage/GameStageList.kt
@@ -0,0 +1,56 @@
+package net.lifecity.mc.skillmaster.game.stage
+
+import net.lifecity.mc.skillmaster.SkillMaster
+import net.lifecity.mc.skillmaster.game.Game
+import net.lifecity.mc.skillmaster.user.SkillUser
+
+class GameStageList {
+    val list = arrayListOf<GameStage>()
+
+    /**
+     * ステージを追加
+     * @param target
+     */
+    fun addStage(target: GameStage) {
+        for(stage in list) {
+            if(stage.name == target.name) return
+        }
+        list.add(target)
+    }
+
+    /**
+     * 名前からステージを取得します
+     * @param name 名前
+     * @return ステージ
+     */
+    fun getFromName(name: String) : GameStage? {
+        for(stage in list) {
+            if(stage.name == name) return stage
+        }
+
+        return null
+    }
+
+    /**
+     * ユーザーが参加しているゲームのフィールドを取得します
+     * @param user ユーザー
+     * @return ユーザーが参加しているゲームのフィールド
+     */
+    fun getFromUser(user: SkillUser) : GameStage? {
+        val game = SkillMaster.instance.gameList.getFromUser(user)
+        return getFromGame(game)
+    }
+
+    /**
+     * 引数のゲームで使用しているフィールドを取得します
+     * @param game ゲーム
+     * @return 使用中のフィールド
+     */
+    fun getFromGame(game: Game) : GameStage? {
+        for(field in list) {
+            if(field.nowGame == game) return field
+        }
+        return null
+    }
+
+}
