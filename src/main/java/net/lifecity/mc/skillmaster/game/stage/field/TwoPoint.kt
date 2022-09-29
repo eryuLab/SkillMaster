8fcc02d4d6288af72933a56e6c22d9ff341f1f02
hirotask <hirotaskhashi@gmail.com>
2022-09-29 23:13:51 +0900

    [update]: TwoPointのKotlin化

--- src/main/java/net/lifecity/mc/skillmaster/game/stage/field/TwoPoint.kt
+++ src/main/java/net/lifecity/mc/skillmaster/game/stage/field/TwoPoint.kt
@@ -1,21 +1,12 @@
-package net.lifecity.mc.skillmaster.game.stage.field;
+package net.lifecity.mc.skillmaster.game.stage.field
 
-import lombok.Getter;
-import net.lifecity.mc.skillmaster.game.stage.FieldType;
-import net.lifecity.mc.skillmaster.game.stage.GameField;
-import net.lifecity.mc.skillmaster.game.stage.GameStage;
-import org.bukkit.Location;
+import net.lifecity.mc.skillmaster.game.stage.FieldType
+import net.lifecity.mc.skillmaster.game.stage.GameField
+import net.lifecity.mc.skillmaster.game.stage.GameStage
+import org.bukkit.Location
 
-public class TwoPoint extends GameField {
-
-    @Getter
-    private final Location pointA;
-    @Getter
-    private final Location pointB;
-
-    public TwoPoint(GameStage field, Location pointA, Location pointB) {
-        super(field, FieldType.TWO_POINT);
-        this.pointA = pointA;
-        this.pointB = pointB;
-    }
-}
+class TwoPoint(
+    field: GameStage?,
+    val pointA: Location,
+    val pointB: Location
+) : GameField(field, FieldType.TWO_POINT)
