5b5e1395b679b8a8d7493a34b8e5e9d428c5b141
hirotask <hirotaskhashi@gmail.com>
2022-09-30 19:29:14 +0900

    [update]: GameResultをKotlin化

--- src/main/java/net/lifecity/mc/skillmaster/game/GameResult.kt
+++ src/main/java/net/lifecity/mc/skillmaster/game/GameResult.kt
@@ -1,16 +1,5 @@
-package net.lifecity.mc.skillmaster.game;
+package net.lifecity.mc.skillmaster.game
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-public enum GameResult {
-    WIN("勝利", "Win"),
-    LOSE("敗北", "Lose"),
-    DRAW("引き分け", "Draw");
-
-    @Getter
-    private String jp;
-    @Getter
-    private String en;
+enum class GameResult(val jp: String, val en: String) {
+    WIN("勝利", "Win"), LOSE("敗北", "Lose"), DRAW("引き分け", "Draw");
 }
