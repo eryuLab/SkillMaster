ad8421ea7a71dc687f980146fadcce425d330ff2
hirotask <hirotaskhashi@gmail.com>
2022-09-30 19:34:21 +0900

    [update]: GameStateをKotlin化

--- src/main/java/net/lifecity/mc/skillmaster/game/GameState.kt
+++ src/main/java/net/lifecity/mc/skillmaster/game/GameState.kt
@@ -1,15 +1,5 @@
-package net.lifecity.mc.skillmaster.game;
+package net.lifecity.mc.skillmaster.game
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-public enum GameState {
-    WAITING_FOR_STARTING("待機中"),
-    COUNT_DOWN("カウントダウン中"),
-    IN_GAMING("ゲーム中"),
-    WAITING_FOR_FINISH("終了処理中");
-
-    @Getter
-    private String jp;
+enum class GameState(val jp: String) {
+    WAITING_FOR_STARTING("待機中"), COUNT_DOWN("カウントダウン中"), IN_GAMING("ゲーム中"), WAITING_FOR_FINISH("終了処理中");
 }
