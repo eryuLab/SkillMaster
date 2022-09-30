e8b93bafe801187ef0cc439154754d2e1822fa5d
hirotask <hirotaskhashi@gmail.com>
2022-09-30 19:28:09 +0900

    [update]: GameTypeをKotlin化

--- src/main/java/net/lifecity/mc/skillmaster/game/GameType.kt
+++ src/main/java/net/lifecity/mc/skillmaster/game/GameType.kt
@@ -1,15 +1,5 @@
-package net.lifecity.mc.skillmaster.game;
+package net.lifecity.mc.skillmaster.game
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-public enum GameType {
-    FREE_FOR_ALL("フリーフォーオール"),
-    DUEL("デュエル"),
-    ONE_ON_ONE("一対一"),
-    TEAM_DEATH_MATCH("チームデスマッチ");
-
-    @Getter
-    private String jp;
+enum class GameType(val jp: String) {
+    FREE_FOR_ALL("フリーフォーオール"), DUEL("デュエル"), ONE_ON_ONE("一対一"), TEAM_DEATH_MATCH("チームデスマッチ");
 }
