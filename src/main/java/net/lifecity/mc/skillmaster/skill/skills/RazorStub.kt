366034c979a19be8b408eba9a8319c87dbb26476
hirotask <hirotaskhashi@gmail.com>
2022-10-15 14:28:20 +0900

    [fix]: 向いてる方向以外に打ち上げられるバグの修正

--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/RazorStub.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/RazorStub.kt
@@ -4,6 +4,8 @@ import net.lifecity.mc.skillmaster.skill.Skill
 import net.lifecity.mc.skillmaster.skill.SkillType
 import net.lifecity.mc.skillmaster.user.SkillUser
 import net.lifecity.mc.skillmaster.weapon.Weapon
+import org.bukkit.ChatColor
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.util.Vector
 
@@ -16,19 +18,43 @@ class RazorStub(user: SkillUser?) : Skill(
     user = user
 ) {
     override fun activate() {
-        if(user == null) return
+        if (user == null) return
 
         val player = this.user.player
-        val world = player.world
+        val target = getTargetEntity(player, player.getNearbyEntities(3.0,3.0,3.0))
 
-        val traceResult = world.rayTraceEntities(player.location, player.location.direction, 2.0)
-        traceResult?.let {
-            val target = it.hitEntity as? Player ?: return
-
-            player.sendMessage("レイザースタブ発動")
-            val vector = Vector(0.0, 2.5, 0.0)
-            target.velocity = vector
+        target?.let {
+            val targetPlayer = target as? Player ?: return
+            if (targetPlayer != player) {
+                user.sendActionBar("${ChatColor.DARK_AQUA} スキル『$name』発動")
+                player.sendMessage("レイザースタブ発動")
+                val vector = Vector(0.0, 1.5, 0.0)
+                targetPlayer.velocity = vector
+                super.deactivate()
+            }
         }
+    }
+
+    /**
+     * @param entity: 始点となるエンティティ
+     * @param entities: 探索対象のEntityのIterableリスト
+     * @return: 探索によって得られたEntity
+     */
+    private fun <T : Entity> getTargetEntity(entity: Entity, entities: Iterable<T>): T? {
+        val threshold = 1.0
+        var target: T? = null
+
+        for (other in entities) {
+            val vec = other.location.toVector().subtract(entity.location.toVector())
+            if (entity.location.direction.normalize().crossProduct(vec).lengthSquared() < threshold
+                && vec.normalize().dot(entity.location.direction.normalize()) >= 0) {
+
+                if(target == null || target.location.distanceSquared(entity.location) > other.location.distanceSquared(entity.location)) {
+                    target = other
+                }
+            }
+        }
+        return target
     }
 
 }
