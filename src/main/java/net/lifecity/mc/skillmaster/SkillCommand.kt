a337b29d27357a4b1a11d09264fe1a57dd01a699
Koryu25 <komu.ryu@icloud.com>
2022-10-16 01:01:50 +0900

    [update]: ゲームモードのトレーニングを開始するコマンドを追加

--- src/main/java/net/lifecity/mc/skillmaster/SkillCommand.kt
+++ src/main/java/net/lifecity/mc/skillmaster/SkillCommand.kt
@@ -7,6 +7,7 @@ import dev.jorel.commandapi.arguments.PlayerArgument
 import dev.jorel.commandapi.executors.CommandExecutor
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import net.lifecity.mc.skillmaster.game.games.Duel
+import net.lifecity.mc.skillmaster.game.games.Training
 import net.lifecity.mc.skillmaster.inventory.SkillInventory
 import net.lifecity.mc.skillmaster.inventory.WeaponInventory
 import net.lifecity.mc.skillmaster.user.UserMode
@@ -87,42 +88,27 @@ object SkillCommand {
                 )
             )
 
-            .then(LiteralArgument("duel")
-                .then(MultiLiteralArgument("闘技場")
-                    .then(PlayerArgument("player1")
-                        .then(PlayerArgument("player2")
+            .then(LiteralArgument("game")
+                .then(LiteralArgument("training")
+                    .then(MultiLiteralArgument("闘技場")
+                        .then(PlayerArgument("player")
                             .executesPlayer(PlayerCommandExecutor {player, args ->
                                 val stageName = args[0] as String
-                                val player1 = args[1] as Player
-                                val player2 = args[2] as Player
+                                val gamePlayer = args[1] as Player
 
-                                //プレイヤー引数確認
-                                if(player1 == player2) {
-                                    player.sendMessage("一人で戦うことはできません")
+                                // ユーザー取得
+                                val user = SkillMaster.INSTANCE.userList.get(gamePlayer) ?: return@PlayerCommandExecutor
+
+                                // プレイヤーがゲーム中か確認
+                                if (SkillMaster.INSTANCE.gameList.inGamingUser(user)) {
+                                    player.sendMessage("${player.name}はすでにゲーム中です")
                                     return@PlayerCommandExecutor
                                 }
 
-                                //ユーザー取得
-                                val user1 = SkillMaster.INSTANCE.userList.get(player1)
-                                val user2 = SkillMaster.INSTANCE.userList.get(player2)
-
-                                if(user1 == null) return@PlayerCommandExecutor
-                                if(user2 == null) return@PlayerCommandExecutor
-
-                                //ゲームがないか確認
-                                if(SkillMaster.INSTANCE.gameList.inGamingUser(user1)) {
-                                    player.sendMessage("${player1.name}はすでにゲーム中です")
-                                    return@PlayerCommandExecutor
-                                }
-                                if(SkillMaster.INSTANCE.gameList.inGamingUser(user2)) {
-                                    player.sendMessage("${player2.name}はすでにゲーム中です")
-                                    return@PlayerCommandExecutor
-                                }
-
-                                //ステージ取得
+                                // ステージ取得
                                 val stage = SkillMaster.INSTANCE.stageList.getFromName(stageName)
 
-                                if(stage == null) {
+                                if (stage == null) {
                                     player.sendMessage("${stageName}は登録されていません")
                                     return@PlayerCommandExecutor
                                 }
@@ -133,10 +119,60 @@ object SkillCommand {
                                     return@PlayerCommandExecutor
                                 }
 
-                                //ゲーム開始
-                                val duel = Duel(stage,user1,user2)
-                                duel.start()
+                                val training = Training(stage, user)
+                                training.start()
                             })
+                        )
+                    )
+                )
+                .then(LiteralArgument("duel")
+                    .then(MultiLiteralArgument("闘技場")
+                        .then(PlayerArgument("player1")
+                            .then(PlayerArgument("player2")
+                                .executesPlayer(PlayerCommandExecutor {player, args ->
+                                    val stageName = args[0] as String
+                                    val player1 = args[1] as Player
+                                    val player2 = args[2] as Player
+
+                                    //プレイヤー引数確認
+                                    if(player1 == player2) {
+                                        player.sendMessage("一人で戦うことはできません")
+                                        return@PlayerCommandExecutor
+                                    }
+
+                                    //ユーザー取得
+                                    val user1 = SkillMaster.INSTANCE.userList.get(player1) ?: return@PlayerCommandExecutor
+                                    val user2 = SkillMaster.INSTANCE.userList.get(player2) ?: return@PlayerCommandExecutor
+
+                                    //ゲームがないか確認
+                                    if(SkillMaster.INSTANCE.gameList.inGamingUser(user1)) {
+                                        player.sendMessage("${player1.name}はすでにゲーム中です")
+                                        return@PlayerCommandExecutor
+                                    }
+                                    if(SkillMaster.INSTANCE.gameList.inGamingUser(user2)) {
+                                        player.sendMessage("${player2.name}はすでにゲーム中です")
+                                        return@PlayerCommandExecutor
+                                    }
+
+                                    //ステージ取得
+                                    val stage = SkillMaster.INSTANCE.stageList.getFromName(stageName)
+
+                                    if(stage == null) {
+                                        player.sendMessage("${stageName}は登録されていません")
+                                        return@PlayerCommandExecutor
+                                    }
+
+                                    //ステージが使用中であるか確認
+                                    if(stage.inUsing()) {
+                                        player.sendMessage("${stageName}は使用中です")
+                                        return@PlayerCommandExecutor
+                                    }
+
+                                    //ゲーム開始
+                                    val duel = Duel(stage,user1,user2)
+                                    duel.start()
+                                })
+                            )
                         )
                     )
                 )
