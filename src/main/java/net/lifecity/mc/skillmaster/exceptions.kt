f0adff4b52b621a373c08d0c82f92be1aa66b9b2
hirotask <hirotaskhashi@gmail.com>
2022-10-18 20:29:11 +0900

    [update]: GameList.kt内のエラーを出すべき個所を修正

--- src/main/java/net/lifecity/mc/skillmaster/exceptions.kt
+++ src/main/java/net/lifecity/mc/skillmaster/exceptions.kt
@@ -1,6 +1,22 @@
 package net.lifecity.mc.skillmaster
 
+/**
+ * 何らかのデータ型を武器に変換できなかった時に発生するエラー
+ */
 class WeaponConvertException(msg: String = "") : Exception(msg)
+
+/**
+ * 何らかのデータ型をスキルに変換できなかった時に発生するエラー
+ */
 class SkillConvertException(msg: String = "") : Exception(msg)
+
+/**
+ * SkillUserがSkillUserListの中に見つからなかった時に発生するエラー
+ */
 class SkillUserNotFoundException(msg: String = "") : Exception(msg)
+
+/**
+ * SkillがSkillSetの中に見つからなかった時に発生するエラー
+ */
+class SkillNotFoundException(msg: String = "") : Exception(msg)
 
