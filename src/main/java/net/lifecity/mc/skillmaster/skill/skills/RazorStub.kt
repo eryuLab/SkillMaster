fdf8e9407fdab13fa75fc67100f2aff109b6c06d
hirotask <hirotaskhashi@gmail.com>
2022-10-18 23:07:43 +0900

    [update]: TargetSearchの作成に合わせてレイザースタブを修正

--- src/main/java/net/lifecity/mc/skillmaster/skill/skills/RazorStub.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/skills/RazorStub.kt
@@ -7,6 +7,7 @@ import net.lifecity.mc.skillmaster.skill.Skill
 import net.lifecity.mc.skillmaster.skill.SkillType
 import net.lifecity.mc.skillmaster.user.SkillUser
 import net.lifecity.mc.skillmaster.utils.DrawParticle
+import net.lifecity.mc.skillmaster.utils.TargetSearch
 import net.lifecity.mc.skillmaster.weapon.Weapon
 import org.bukkit.ChatColor
 import org.bukkit.Location
@@ -30,7 +31,8 @@ class RazorStub(user: SkillUser?) : Skill(
         if (user == null) return
 
         val player = this.user.player
-        val target = getTargetEntity(player, player.getNearbyEntities(3.0, 3.0, 3.0))
+        val targetSearch = TargetSearch()
+        val target = targetSearch.getTargetEntity(player, 3.0)
 
         target?.let {
             val livingTarget = target as? LivingEntity ?: return
@@ -44,37 +46,6 @@ class RazorStub(user: SkillUser?) : Skill(
                 livingTarget.damage(5.0)
             }
         }
-    }
-
-    /**
-     * @param entity: 始点となるエンティティ
-     * @param entities: 探索対象のEntityのIterableリスト
-     * @return: 探索によって得られたEntity
-     */
-    private fun <T : Entity> getTargetEntity(entity: Entity, entities: Iterable<T>): T? {
-        val threshold = 1.0
-        var target: T? = null
-
-        for (other in entities) { //iterableリストの中を走査
-            //自分のベクトルと、対象のベクトルの差を計算 => 相手が向いている方向と自分が向いている方向が逆ならば最大, 同じならば最小
-            val vec = other.location.toVector().subtract(entity.location.toVector())
-
-            // 自分のベクトルと、vecの外積の長さが1未満である => 自分のベクトルとvecがなす平行四辺形の面積が1未満、
-            // つまりここでもどの程度同じ方向を向いているか判定している。
-            // かつvecと自分のベクトルの内積が0以上 => 自分のベクトルがvecの向いている方向と同じ方向
-            // であるならば
-            if (entity.location.direction.normalize().crossProduct(vec).lengthSquared() < threshold
-                && vec.normalize().dot(entity.location.direction.normalize()) >= 0
-            ) {
-                if (target == null || target.location.distanceSquared(entity.location) > other.location.distanceSquared(
-                        entity.location
-                    )
-                ) {
-                    target = other
-                }
-            }
-        }
-        return target
     }
 
     /**
