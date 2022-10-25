31b19edba6e4b1cd6de7eda5b8788034a78ddfe6
hirotask <hirotaskhashi@gmail.com>
2022-10-25 23:05:22 +0900

    [update]: Messagerクラスにメソッドを追加

--- src/main/java/net/lifecity/mc/skillmaster/utils/Messager.kt
+++ src/main/java/net/lifecity/mc/skillmaster/utils/Messager.kt
@@ -1,5 +1,6 @@
 package net.lifecity.mc.skillmaster.utils
 
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor.*
 import org.bukkit.entity.Player
 
@@ -7,5 +8,23 @@ import org.bukkit.entity.Player
 object Messager {
     fun sendAlertMessage(player: Player, msg: String) {
         player.sendMessage("$WHITE[${RED}警告${WHITE}]: $msg")
+    }
+
+    fun sendInformationMessage(msg: String) {
+        for (player in Bukkit.getServer().onlinePlayers) {
+            player.sendMessage("${WHITE}[${GOLD}お知らせ${WHITE}]: $msg")
+        }
+    }
+
+    fun sendDebugMessage(player: Player, msg: String) {
+        player.sendMessage("${WHITE}[${YELLOW}デバッグ${WHITE}]: $msg")
+    }
+
+    fun sendErrorMessage(player: Player, msg: String) {
+        player.sendMessage("${RED}[エラー]: $msg")
+    }
+
+    fun sendLogMessage(player: Player, msg: String) {
+        player.sendMessage("${WHITE}[ログ]: $msg")
     }
 }
