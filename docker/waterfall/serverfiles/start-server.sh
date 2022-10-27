33b6ff385e465878fc7beca0a1d58506d57ae0b7
hirotask <hirotaskhashi@gmail.com>
2022-10-27 22:47:48 +0900

    [add]: waterfall/start-server.shの追加

---
+++ docker/waterfall/serverfiles/start-server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+rm -r /waterfall/plugins/ || true
+
+# overwrite server directory
+cp -Rf /waterfall-files/* /waterfall/
+
+cd /waterfall/ && java -jar /waterfall/waterfall.jar
+
