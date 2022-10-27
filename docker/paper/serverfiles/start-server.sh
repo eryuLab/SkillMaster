7f20d80017514dca9257fd300447160af369615f
hirotask <hirotaskhashi@gmail.com>
2022-10-27 21:08:59 +0900

    [fix]: start-server.shのフォルダ名を変更

--- docker/paper/serverfiles/start-server.sh
+++ docker/paper/serverfiles/start-server.sh
@@ -2,19 +2,8 @@
 
 set -e
 
-cd /spigot/
-
-JMX_PORT=${JMX_PORT:-7091}
-JMX_BINDING=${JMX_BINDING:-0.0.0.0}
-JMX_HOST=${JMX_HOST:-localhost}
+cd /paper/
 
 java \
-  -Dcom.sun.management.jmxremote.local.only=false \
-  -Dcom.sun.management.jmxremote.port=${JMX_PORT} \
-  -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT} \
-  -Dcom.sun.management.jmxremote.authenticate=false \
-  -Dcom.sun.management.jmxremote.ssl=false \
-  -Dcom.sun.management.jmxremote.host=${JMX_BINDING} \
-  -Djava.rmi.server.hostname=${JMX_HOST} \
-  -Xmx4g -Xms256m \
-  -jar /spigot/spigot*.jar nogui
+  -Xmx2g -Xms256m \
+  -jar /paper/paper*.jar nogui
