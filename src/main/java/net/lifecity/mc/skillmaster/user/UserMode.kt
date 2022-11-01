acae59ad0a6d286ab87f187f6cc4abf47c128ff6
Koryu25 <komu.ryu@icloud.com>
2022-11-01 15:07:15 +0900

    [update]: UserModeをobjectに変更

--- src/main/java/net/lifecity/mc/skillmaster/user/UserMode.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/UserMode.kt
@@ -2,56 +2,56 @@ package net.lifecity.mc.skillmaster.user
 
 import net.lifecity.mc.skillmaster.inventory.UserInventory
 
-sealed class UserMode(val name: String, val user: SkillUser) {
+sealed class UserMode(val name: String) {
 
-    fun onShift(mode: UserMode) {
+    fun onShift(user: SkillUser, mode: UserMode) {
         when (mode) {
-            is Battle -> toBattle()
-            is Training -> toTraining()
-            is UnArmed -> toUnArmed()
+            is Battle -> toBattle(user)
+            is Training -> toTraining(user)
+            is UnArmed -> toUnArmed(user)
         }
     }
-    abstract fun toBattle()
-    abstract fun toTraining()
-    abstract fun toUnArmed()
+    abstract fun toBattle(user: SkillUser)
+    abstract fun toTraining(user: SkillUser)
+    abstract fun toUnArmed(user: SkillUser)
 
-    protected fun initSkills() {
+    protected fun initSkills(user: SkillUser) {
         user.initSkills()
     }
-    protected fun initInventory() {
+    protected fun initInventory(user: SkillUser) {
         user.userInventory = UserInventory(user)
     }
-    protected fun initHP() {
+    protected fun initHP(user: SkillUser) {
         user.player.maxHealth = 40.0
         user.player.health = 40.0
     }
-    protected fun clearInventory() {
+    protected fun clearInventory(user: SkillUser) {
         for (index in 9..35) {
             user.player.inventory.clear(index)
         }
     }
 
-    class Battle(user: SkillUser): UserMode("バトル", user) {
-        override fun toBattle() {}
-        override fun toTraining() = initSkills()
-        override fun toUnArmed() = clearInventory()
+    object Battle : UserMode("バトル") {
+        override fun toBattle(user: SkillUser) {}
+        override fun toTraining(user: SkillUser) = initSkills(user)
+        override fun toUnArmed(user: SkillUser) = clearInventory(user)
     }
-    class Training(user: SkillUser): UserMode("トレーニング", user) {
-        override fun toBattle() = initSkills()
-        override fun toTraining() {}
-        override fun toUnArmed() = clearInventory()
+    object Training : UserMode("トレーニング") {
+        override fun toBattle(user: SkillUser) = initSkills(user)
+        override fun toTraining(user: SkillUser) {}
+        override fun toUnArmed(user: SkillUser) = clearInventory(user)
     }
-    class UnArmed(user: SkillUser): UserMode("武装解除", user) {
-        override fun toBattle() {
-            initSkills()
-            initInventory()
-            initHP()
+    object UnArmed : UserMode("武装解除") {
+        override fun toBattle(user: SkillUser) {
+            initSkills(user)
+            initInventory(user)
+            initHP(user)
         }
-        override fun toTraining() {
-            initSkills()
-            initInventory()
-            initHP()
+        override fun toTraining(user: SkillUser) {
+            initSkills(user)
+            initInventory(user)
+            initHP(user)
         }
-        override fun toUnArmed() {}
+        override fun toUnArmed(user: SkillUser) {}
     }
 }
