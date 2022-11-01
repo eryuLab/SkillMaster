237846a600425781698fbd14775cb364c8092a65
Koryu25 <komu.ryu@icloud.com>
2022-11-01 14:57:33 +0900

    [update]: UserModeをsealed classに変更

--- src/main/java/net/lifecity/mc/skillmaster/user/UserMode.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/UserMode.kt
@@ -1,8 +1,57 @@
 package net.lifecity.mc.skillmaster.user
 
-/**
- * SkillUserのモード
- */
-enum class UserMode(val jp: String) {
-    BATTLE("バトル"), TRAINING("トレーニング"), UNARMED("武装解除")
+import net.lifecity.mc.skillmaster.inventory.UserInventory
+
+sealed class UserMode(val name: String, val user: SkillUser) {
+
+    fun onShift(mode: UserMode) {
+        when (mode) {
+            is Battle -> toBattle()
+            is Training -> toTraining()
+            is UnArmed -> toUnArmed()
+        }
+    }
+    abstract fun toBattle()
+    abstract fun toTraining()
+    abstract fun toUnArmed()
+
+    protected fun initSkills() {
+        user.initSkills()
+    }
+    protected fun initInventory() {
+        user.userInventory = UserInventory(user)
+    }
+    protected fun initHP() {
+        user.player.maxHealth = 40.0
+        user.player.health = 40.0
+    }
+    protected fun clearInventory() {
+        for (index in 9..35) {
+            user.player.inventory.clear(index)
+        }
+    }
+
+    class Battle(user: SkillUser): UserMode("バトル", user) {
+        override fun toBattle() {}
+        override fun toTraining() = initSkills()
+        override fun toUnArmed() = clearInventory()
+    }
+    class Training(user: SkillUser): UserMode("トレーニング", user) {
+        override fun toBattle() = initSkills()
+        override fun toTraining() {}
+        override fun toUnArmed() = clearInventory()
+    }
+    class UnArmed(user: SkillUser): UserMode("武装解除", user) {
+        override fun toBattle() {
+            initSkills()
+            initInventory()
+            initHP()
+        }
+        override fun toTraining() {
+            initSkills()
+            initInventory()
+            initHP()
+        }
+        override fun toUnArmed() {}
+    }
 }
