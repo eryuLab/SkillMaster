40ea811d5c82b64f2d25e22368a0465473e206e4
hirotask <hirotaskhashi@gmail.com>
2022-11-05 11:40:46 +0900

    [remove]: GameType.ktの削除

--- src/main/java/net/lifecity/mc/skillmaster/game/GameType.kt
+++
@@ -1,9 +0,0 @@
-package net.lifecity.mc.skillmaster.game
-
-enum class GameType(val jp: String) {
-    TRAINING("トレーニング"),
-    FREE_FOR_ALL("フリーフォーオール"),
-    DUEL("デュエル"),
-    ONE_ON_ONE("一対一"),
-    TEAM_DEATH_MATCH("チームデスマッチ");
-}
