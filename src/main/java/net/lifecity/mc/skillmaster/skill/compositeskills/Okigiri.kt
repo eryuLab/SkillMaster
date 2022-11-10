c13f9451393d8226cd684e3b08952e4df95698de
Koryu25 <komu.ryu@icloud.com>
2022-11-10 10:28:59 +0900

    [update]: 攻撃時のSEとLEを実装

--- src/main/java/net/lifecity/mc/skillmaster/skill/compositeskills/Okigiri.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/compositeskills/Okigiri.kt
@@ -1,6 +1,8 @@
 package net.lifecity.mc.skillmaster.skill.compositeskills
 
+import com.github.syari.spigot.api.particle.spawnParticle
 import com.github.syari.spigot.api.scheduler.runTaskTimer
+import com.github.syari.spigot.api.sound.playSound
 import net.lifecity.mc.skillmaster.SkillMaster
 import net.lifecity.mc.skillmaster.skill.CompositeSkill
 import net.lifecity.mc.skillmaster.skill.SkillType
@@ -8,6 +10,9 @@ import net.lifecity.mc.skillmaster.skill.function.Attack
 import net.lifecity.mc.skillmaster.user.SkillUser
 import net.lifecity.mc.skillmaster.utils.NearTargets
 import net.lifecity.mc.skillmaster.weapon.Weapon
+import org.bukkit.Particle
+import org.bukkit.Sound
+import org.bukkit.SoundCategory
 import org.bukkit.entity.LivingEntity
 
 class Okigiri(user: SkillUser): CompositeSkill(
@@ -34,11 +39,27 @@ class Okigiri(user: SkillUser): CompositeSkill(
             if (!activated)
                 cancel()
 
+            // SEとLE
+
+
             // 攻撃対象を取得
             val targets = NearTargets.search(user.player, 1.4)
             if (targets.isNotEmpty()) {
                 val target = targets[0]
                 attackAddVector(user, target, 3.5, user.player.eyeLocation.direction.setY(0.15).multiply(0.35))
+
+                // SEとLE
+                var effectCount = 0
+                SkillMaster.INSTANCE.runTaskTimer(3) {
+                    if (effectCount > 4) {
+                        cancel()
+                    }
+
+                    target.location.spawnParticle(Particle.SWEEP_ATTACK, 3, 0.3, 0.3, 0.3)
+                    target.location.playSound(Sound.ENTITY_PLAYER_ATTACK_SWEEP)
+
+                    effectCount++
+                }
                 cancel()
             }
         }
