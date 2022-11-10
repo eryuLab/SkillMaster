55931e3144cf202d27a322e09ba78d67d054133b
Koryu25 <komu.ryu@icloud.com>
2022-11-10 11:30:46 +0900

    [update]: 発動時のSEとLEを実装

--- src/main/java/net/lifecity/mc/skillmaster/skill/compositeskills/Okigiri.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/compositeskills/Okigiri.kt
@@ -5,9 +5,11 @@ import com.github.syari.spigot.api.scheduler.runTaskTimer
 import com.github.syari.spigot.api.sound.playSound
 import net.lifecity.mc.skillmaster.SkillMaster
 import net.lifecity.mc.skillmaster.skill.CompositeSkill
+import net.lifecity.mc.skillmaster.skill.Skill
 import net.lifecity.mc.skillmaster.skill.SkillType
 import net.lifecity.mc.skillmaster.skill.function.Attack
 import net.lifecity.mc.skillmaster.user.SkillUser
+import net.lifecity.mc.skillmaster.utils.DrawParticle
 import net.lifecity.mc.skillmaster.utils.NearTargets
 import net.lifecity.mc.skillmaster.weapon.Weapon
 import org.bukkit.Particle
@@ -40,7 +42,25 @@ class Okigiri(user: SkillUser): CompositeSkill(
                 cancel()
 
             // SEとLE
+            user.player.location.playSound(Sound.ENTITY_SPLASH_POTION_THROW, pitch = 0.2f)
+            val draw = DrawParticle()
+            var count1 = 0
+            SkillMaster.INSTANCE.runTaskTimer(8) {
+                if (count1 >= 2)
+                    cancel()
 
+                draw.drawCircle(
+                        user.player.location.add(0.0, 1.0, 0.0),
+                        Particle.CRIT,
+                        radius = 1.8,
+                        points = 17,
+                        rotX = Math.PI * 90.0 / 180.0,
+                        rotY = Math.PI * (user.player.location.yaw / 180.0) * -1,
+                        count = 4
+                )
+
+                count1++
+            }
 
             // 攻撃対象を取得
             val targets = NearTargets.search(user.player, 1.4)
