b822760979d2b3d17c5a0766fc91ad8abb3d9d9c
hirotask <hirotaskhashi@gmail.com>
2022-11-13 01:01:19 +0900

    [update]:　RankBoardに各種メソッドを追加

--- src/main/java/net/lifecity/mc/skillmaster/rankboard/RankBoard.kt
+++ src/main/java/net/lifecity/mc/skillmaster/rankboard/RankBoard.kt
@@ -1,16 +1,35 @@
 package net.lifecity.mc.skillmaster.rankboard
 
+import com.gmail.filoghost.holographicdisplays.api.Hologram
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI
 import net.lifecity.mc.skillmaster.SkillMaster
+import org.bukkit.ChatColor
 import org.bukkit.Location
 
 class RankBoard {
 
-    fun spawn(location: Location, text: List<String>) {
-        val hologram = HologramsAPI.createHologram(SkillMaster.INSTANCE, location.add(0.0, 2.0, 0.0))
-        hologram.appendTextLine(">>>>> Rank Board <<<<<")
+    private fun setRankBoardTexts(hologram: Hologram, text: List<String>) {
+        hologram.appendTextLine("${ChatColor.YELLOW}>>>>> Rank Board <<<<<")
         for ((index,t) in text.withIndex()) {
-            hologram.appendTextLine("${index + 1}. $t")
+            hologram.appendTextLine("${ChatColor.YELLOW}${index + 1}${ChatColor.GREEN}. $t")
         }
+    }
+
+    fun create(location: Location, text: List<String>) : Hologram {
+        val hologram = HologramsAPI.createHologram(SkillMaster.INSTANCE, location.add(0.0, 2.0, 0.0))
+        setRankBoardTexts(hologram, text)
+
+        return hologram
+    }
+
+    fun update(hologram: Hologram, text: List<String>) : Hologram {
+        hologram.clearLines()
+        setRankBoardTexts(hologram, text)
+
+        return hologram
+    }
+
+    fun delete(hologram: Hologram) {
+        hologram.delete()
     }
 }
