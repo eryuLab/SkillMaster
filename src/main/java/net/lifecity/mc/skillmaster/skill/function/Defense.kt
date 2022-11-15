7b5f0108711f639aceca755b9df77b2a771a8b05
Koryu25 <komu.ryu@icloud.com>
2022-11-15 09:22:04 +0900

    [update]: Defenseにダメージメソッドを追加

--- src/main/java/net/lifecity/mc/skillmaster/skill/function/Defense.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/function/Defense.kt
@@ -1,9 +1,20 @@
 package net.lifecity.mc.skillmaster.skill.function
 
+import net.lifecity.mc.skillmaster.user.SkillUser
 import org.bukkit.Location
 import org.bukkit.util.Vector
 
 interface Defense {
 
     fun defense(damage: Double, vector: Vector, atkLoc: Location)
+
+    fun damageAddVector(user: SkillUser, damage: Double, vector: Vector) {
+        user.player.damage(damage)
+        user.player.velocity.add(vector)
+    }
+
+    fun damageChangeVector(user: SkillUser, damage: Double, vector: Vector) {
+        user.player.damage(damage)
+        user.player.velocity = vector
+    }
 }
