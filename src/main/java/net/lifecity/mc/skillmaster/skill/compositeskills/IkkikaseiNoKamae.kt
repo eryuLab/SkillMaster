cf52cf48db2f526240d678530961a1b047b07e1c
Koryu25 <komu.ryu@icloud.com>
2022-11-15 13:15:19 +0900

    [update]: エフェクトを実装

--- src/main/java/net/lifecity/mc/skillmaster/skill/compositeskills/IkkikaseiNoKamae.kt
+++ src/main/java/net/lifecity/mc/skillmaster/skill/compositeskills/IkkikaseiNoKamae.kt
@@ -1,5 +1,6 @@
 package net.lifecity.mc.skillmaster.skill.compositeskills
 
+import com.github.syari.spigot.api.particle.spawnParticle
 import com.github.syari.spigot.api.scheduler.runTaskLater
 import com.github.syari.spigot.api.scheduler.runTaskTimer
 import com.github.syari.spigot.api.sound.playSound
@@ -8,11 +9,15 @@ import net.lifecity.mc.skillmaster.skill.CompositeSkill
 import net.lifecity.mc.skillmaster.skill.SkillType
 import net.lifecity.mc.skillmaster.skill.function.Defense
 import net.lifecity.mc.skillmaster.user.SkillUser
+import net.lifecity.mc.skillmaster.utils.DrawParticle
 import net.lifecity.mc.skillmaster.utils.TargetSearch
 import net.lifecity.mc.skillmaster.weapon.Weapon
+import org.bukkit.Color
 import org.bukkit.Location
+import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.util.Vector
+import kotlin.random.Random
 
 class IkkikaseiNoKamae(user: SkillUser): CompositeSkill(
     "一気呵成の構え",
@@ -26,8 +31,17 @@ class IkkikaseiNoKamae(user: SkillUser): CompositeSkill(
 ), Defense {
     override fun onActivate() {
         // SE
+        user.player.location.playSound(Sound.ENTITY_CREEPER_DEATH, pitch = 1.5f)
 
         // LE
+        DrawParticle().drawCircle(
+            user.player.location.add(0.0, 1.0, 0.0),
+            Particle.CRIT,
+            radius = 1.9,
+            points = 13,
+            count = 3,
+            speed = 0.02
+        )
     }
 
     override fun defense(damage: Double, vector: Vector, atkLoc: Location) {
@@ -46,6 +60,19 @@ class IkkikaseiNoKamae(user: SkillUser): CompositeSkill(
         }
 
         // LE
-
+        val draw = DrawParticle()
+        // 円
+        draw.drawCircle(
+            user.player.location.add(0.0, Random.nextDouble(-0.5, 0.5), 0.0),
+            Particle.DUST_COLOR_TRANSITION,
+            Particle.DustTransition(Color.GRAY, Color.WHITE, 0.8f),
+            1.7,
+            20,
+            Math.PI * Random.nextDouble() * 0.1
+        )
+        // 攻撃座標方向にエフェクト
+        val sub = user.player.location.subtract(atkLoc).toVector()
+        val loc = user.player.location.add(sub)
+        loc.spawnParticle(Particle.SWEEP_ATTACK, 3)
     }
 }
