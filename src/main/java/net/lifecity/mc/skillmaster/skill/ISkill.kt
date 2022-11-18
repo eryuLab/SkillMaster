a2273b7f2e0a1f532ad4e688baa1766ae752a0a4
hirotask <hirotaskhashi@gmail.com>
2022-11-18 19:42:05 +0900

    [add]:　Skillインターフェースの追加

---
+++ src/main/java/net/lifecity/mc/skillmaster/skill/ISkill.kt
@@ -0,0 +1,11 @@
+package net.lifecity.mc.skillmaster.skill
+
+import net.lifecity.mc.skillmaster.weapon.Weapon
+
+interface ISkill {
+    val name: String
+    val weaponList: List<Weapon>
+    val type: SkillType
+    val lore: List<String>
+    val interval: Int
+}
