f5c820ecf148a0d53c35754fd4d7f23034f5366f
Koryu25 <komu.ryu@icloud.com>
2022-11-28 15:04:06 +0900

    [update]: FileUtilの変更をロールバック

--- src/main/java/net/lifecity/mc/skillmaster/utils/FileUtil.kt
+++ src/main/java/net/lifecity/mc/skillmaster/utils/FileUtil.kt
@@ -2,8 +2,6 @@ package net.lifecity.mc.skillmaster.utils
 
 import net.lifecity.mc.skillmaster.SkillMaster
 import org.apache.commons.io.FileUtils
-import org.bukkit.Location
-import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
 import java.io.IOException
 
@@ -20,15 +18,6 @@ class FileUtil {
     )
 
     fun init() {
-        val file = File(SkillMaster.INSTANCE.dataFolder, "data/sign.yml")
-        val stream = SkillMaster.INSTANCE.getResource("data/sign.yml")
-        if (!file.exists() && stream != null) {
-            try {
-                FileUtils.copyInputStreamToFile(stream, file)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
         for (fileName in schems) {
             val stream = SkillMaster.INSTANCE.getResource("schematics/$fileName")
             if (stream != null) {
@@ -39,40 +28,6 @@ class FileUtil {
                     e.printStackTrace()
                 }
             }
-        }
-    }
-
-    fun load() {
-        try {
-            val config = YamlConfiguration()
-            config.load("data/sign.yml")
-            val keys = config.getConfigurationSection("sign")?.getKeys(false)
-            if (keys != null) {
-                for (key in keys) {
-                    val loc = config.getLocation("sign.$key")
-                    if (loc != null)
-                        SkillMaster.INSTANCE.signList.add(loc)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    fun save() {
-        try {
-            val file = File(SkillMaster.INSTANCE.dataFolder, "data/sign.yml")
-
-            val config = YamlConfiguration()
-
-            for ((loc, index) in SkillMaster.INSTANCE.signList.list.withIndex()) {
-                config.set("sign.$index", loc)
-            }
-
-            config.save(file)
-
-        } catch (e: IOException) {
-            e.printStackTrace()
         }
     }
 }
