280687ff56e19667d99fa37da3febd9ab5b1c3c1
Koryu25 <komu.ryu@icloud.com>
2022-11-28 18:48:00 +0900

    [update]: ロードとセーブの処理を追加

--- src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SignConfig.kt
+++ src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SignConfig.kt
@@ -1,12 +1,26 @@
 package net.lifecity.mc.skillmaster.utils.file.data
 
+import net.lifecity.mc.skillmaster.SkillMaster
+
 class SignConfig: CustomConfig("data/sign.yml") {
 
     fun onEnable() {
         // ロード
+        val keys = config?.getConfigurationSection("sign")?.getKeys(false)
+        if (keys != null) {
+            for (key in keys) {
+                val location = config?.getLocation("sign.${key}") ?: continue
+                SkillMaster.INSTANCE.signList.add(location)
+            }
+        }
     }
 
     fun onDisable() {
         // セーブ
+        val list = SkillMaster.INSTANCE.signList.list
+        for ((loc, index) in list.withIndex()) {
+            config?.set("sign.${index}", loc)
+        }
+        save()
     }
 }
