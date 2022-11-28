deea00a528ae13b6def67a6e820333d2672610aa
Koryu25 <komu.ryu@icloud.com>
2022-11-28 16:55:07 +0900

    [add]: SignConfigを追加

---
+++ src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SignConfig.kt
@@ -0,0 +1,12 @@
+package net.lifecity.mc.skillmaster.utils.file.data
+
+class SignConfig: CustomConfig("data/sign.yml") {
+
+    fun onEnable() {
+        // ロード
+    }
+
+    fun onDisable() {
+        // セーブ
+    }
+}
