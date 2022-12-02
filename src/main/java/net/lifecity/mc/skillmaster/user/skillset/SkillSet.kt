64ba33ef5f27e975e436ab31b549aa5e565f9a9c
Koryu25 <komu.ryu@icloud.com>
2022-12-02 22:45:38 +0900

    [rename]: UserSkillSetをSkillSetに改名

--- src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
+++ src/main/java/net/lifecity/mc/skillmaster/user/skillset/SkillSet.kt
@@ -1,12 +1,8 @@
 package net.lifecity.mc.skillmaster.user.skillset
 
-import net.lifecity.mc.skillmaster.user.SkillUser
-
-data class UserSkillSet(
-    val user: SkillUser,
-    val number: Int,
+data class SkillSet(
+    val number: Int,// 0で現在のスキルセット
     var rightCard: SkillCard = SkillCard(SkillButton.RIGHT),
     var swapCard: SkillCard = SkillCard(SkillButton.SWAP),
     var dropCard: SkillCard = SkillCard(SkillButton.DROP)
-) {
-}
+)
