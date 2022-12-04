e187e5d8fb48d146d6c5cc59c3460304ae6cbe09
Koryu25 <komu.ryu@icloud.com>
2022-12-04 13:19:35 +0900

    [update]: onPlayerJoin()の追加

--- src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SkillSetConfig.kt
+++ src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SkillSetConfig.kt
@@ -1,4 +1,34 @@
 package net.lifecity.mc.skillmaster.utils.file.data
 
+import net.lifecity.mc.skillmaster.skill.Skill
+import net.lifecity.mc.skillmaster.skill.SkillManager
+import net.lifecity.mc.skillmaster.user.SkillUser
+import net.lifecity.mc.skillmaster.user.skillset.SkillSet
+import org.bukkit.entity.Player
+
 class SkillSetConfig: CustomConfig("data/skillset.yml") {
+
+
+    companion object {
+        private val KEY_NAMES: List<String> = arrayListOf("r", "s", "d")
+    }
+
+    fun onPlayerJoin(user: SkillUser) {
+        val playerName = user.player.name
+        val sm = SkillManager(user)
+        for (setNum in 0..4) {
+            val skillSet = user.skillSetList[setNum]
+            key@for (key in skillSet.keyList) {
+                val skillClassName = config?.getString(
+                    "skill-set.${playerName}.'${setNum}'.${key.button.systemName}${key.number}"
+                ) ?: continue@key
+                val skill = sm.fromClassName(skillClassName)
+                key.skill = skill
+            }
+        }
+    }
+
+    fun onPlayerLeft(player: Player) {
+
+    }
 }
