99db8b5dc2e403761f7864dd0686060c17b2e643
Koryu25 <komu.ryu@icloud.com>
2022-12-04 15:07:40 +0900

    [fix]: 数字のクォーテーションを修正

--- src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SkillSetConfig.kt
+++ src/main/java/net/lifecity/mc/skillmaster/utils/file/data/SkillSetConfig.kt
@@ -1,26 +1,23 @@
 package net.lifecity.mc.skillmaster.utils.file.data
 
-import net.lifecity.mc.skillmaster.skill.Skill
 import net.lifecity.mc.skillmaster.skill.SkillManager
 import net.lifecity.mc.skillmaster.user.SkillUser
-import net.lifecity.mc.skillmaster.user.skillset.SkillSet
-import org.bukkit.entity.Player
+import org.bukkit.Bukkit
+import java.util.logging.Level
 
 class SkillSetConfig: CustomConfig("data/skillset.yml") {
-
-
-    companion object {
-        private val KEY_NAMES: List<String> = arrayListOf("r", "s", "d")
-    }
 
     fun onPlayerJoin(user: SkillUser) {
         val playerName = user.player.name
         val sm = SkillManager(user)
+
         for (setNum in 0..4) {
             val skillSet = user.skillSetList[setNum]
+
             key@for (key in skillSet.keyList) {
+
                 val skillClassName = config?.getString(
-                    "skill-set.${playerName}.'${setNum}'.${key.button.systemName}${key.number}"
+                    "skill-set.${playerName}.${setNum}.${key.button.systemName}${key.number}"
                 ) ?: continue@key
                 val skill = sm.fromClassName(skillClassName)
                 key.skill = skill
@@ -30,12 +27,14 @@ class SkillSetConfig: CustomConfig("data/skillset.yml") {
 
     fun onPlayerQuit(user: SkillUser) {
         val playerName = user.player.name
+
         for (setNum in 0..4) {
             val skillSet = user.skillSetList[setNum]
+
             key@for (key in skillSet.keyList) {
                 val skill = key.skill ?: continue@key
                 val skillClassName = skill::class.java.simpleName
-                config?.set("skill-set.${playerName}.'${setNum}'.${key.button.systemName}${key.number}", skillClassName)
+                config?.set("skill-set.${playerName}.${setNum}.${key.button.systemName}${key.number}", skillClassName)
             }
         }
         save()
