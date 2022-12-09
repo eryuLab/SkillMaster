0dca1c60d7b0e45945b84af51438231bd3e7dcbb
Koryu25 <komu.ryu@icloud.com>
2022-12-09 23:24:37 +0900

    [add]: AttackDataの追加

---
+++ src/main/java/net/lifecity/mc/skillmaster/skill/data/AttackData.kt
@@ -0,0 +1,12 @@
+package net.lifecity.mc.skillmaster.skill.data
+
+import net.lifecity.mc.skillmaster.user.SkillUser
+import org.bukkit.Location
+import org.bukkit.util.Vector
+
+data class AttackData(
+    val attacker: SkillUser,
+    val damage: Double,
+    val vector: Vector,
+    val attackLocation: Location
+) {}
